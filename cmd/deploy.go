4b87e2b79c9b22ddb3601798e63feb9ac0c646f9
Connor Daly <connor.daly@avalabs.org>
2022-04-25 17:47:00 -0400

    Added basic entry for deploy

--- cmd/deploy.go
+++ cmd/deploy.go
@@ -5,8 +5,16 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
 
+	"github.com/ava-labs/avalanche-cli/cmd/prompts"
+	"github.com/ava-labs/avalanche-cli/pkg/models"
 	"github.com/spf13/cobra"
 	// "github.com/ava-labs/avalanche-network-runner/cmd/avalanche-network-runner/server"
 )
@@ -25,6 +33,9 @@ to quickly create a Cobra application.`,
 	Args: cobra.ExactArgs(1),
 }
 
+var deployLocal *bool
+var force *bool
+
 func init() {
 	subnetCmd.AddCommand(deployCmd)
 
@@ -36,12 +47,71 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// deployCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	deployLocal = deployCmd.Flags().BoolP("local", "l", false, "Deploy subnet locally")
+	force = deployCmd.Flags().BoolP("force", "f", false, "Deploy without asking for confirmation")
+}
+
+func getChainsInSubnet(subnetName string) ([]string, error) {
+	usr, _ := user.Current()
+	mainDir := filepath.Join(usr.HomeDir, BaseDir)
+	files, err := ioutil.ReadDir(mainDir)
+	if err != nil {
+		return []string{}, err
+	}
+
+	chains := []string{}
+
+	for _, f := range files {
+		if strings.Contains(f.Name(), sidecar_suffix) {
+			// read in sidecar file
+			path := filepath.Join(mainDir, f.Name())
+			jsonBytes, err := os.ReadFile(path)
+			if err != nil {
+				return []string{}, err
+			}
+
+			var sc models.Sidecar
+			err = json.Unmarshal(jsonBytes, &sc)
+			if err != nil {
+				return []string{}, err
+			}
+			if sc.Subnet == subnetName {
+				chains = append(chains, sc.Name)
+			}
+		}
+	}
+	return chains, nil
 }
 
 func deploySubnet(cmd *cobra.Command, args []string) {
-	fmt.Println("deploy called")
-	// nrCmd := cobra.Command{}
+	// Check subnet exists
+	// TODO create a file that lists chains by subnet for fast querying
+	chains, err := getChainsInSubnet(args[0])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	// server.serverFunc(nrCmd, []string{})
+	if len(chains) == 0 {
+		fmt.Println("Invalid subnet", args[0])
+		return
+	}
+
+	var network models.Network
+	if *deployLocal {
+		network = models.Local
+	} else {
+		networkStr, err := prompts.CaptureList(
+			"Choose a network to deploy on",
+			[]string{models.Local.String(), models.Fuji.String(), models.Mainnet.String()},
+		)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		network = models.NetworkFromString(networkStr)
+	}
+
+	fmt.Println("Deploying", chains, "to", network.String())
+
 }
---
+++ pkg/models/network.go
@@ -0,0 +1,34 @@
+package models
+
+type Network int64
+
+const (
+	Undefined Network = iota
+	Mainnet
+	Fuji
+	Local
+)
+
+func (s Network) String() string {
+	switch s {
+	case Mainnet:
+		return "Mainnet"
+	case Fuji:
+		return "Fuji"
+	case Local:
+		return "Local Network"
+	}
+	return "Unknown Network"
+}
+
+func NetworkFromString(s string) Network {
+	switch s {
+	case Mainnet.String():
+		return Mainnet
+	case Fuji.String():
+		return Fuji
+	case Local.String():
+		return Local
+	}
+	return Undefined
+}
