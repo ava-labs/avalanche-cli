feaa320afeac9cccf610ae37a9eed8bd3a5cb3c3
Fabio Barone <holistic.computing@gmail.com>
2022-05-27 10:41:28 -0500

    remove TODO with hardcoded IP

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -8,10 +8,8 @@ const (
 	DefaultPerms755 = 0o755
 
 	LatestAvagoReleaseURL = "https://api.github.com/repos/ava-labs/avalanchego/releases/latest"
-	// TODO we can not release with this...
-	BinaryServerURL    = "http://3.84.91.164:8998"
-	ServerRunFile      = "/tmp/gRPCserver.run"
-	AvalancheCliBinDir = "bin"
+	ServerRunFile         = "/tmp/gRPCserver.run"
+	AvalancheCliBinDir    = "bin"
 
 	RequestTimeout = 3 * time.Minute
 )
