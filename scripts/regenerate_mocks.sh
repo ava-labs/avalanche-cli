b569d9858968a85962e6b13aa5421494d1f7b47f
Connor Daly <connor.daly@avalabs.org>
2022-07-15 15:58:37 -0400

    Fixed file case

--- scripts/regenerate_mocks.sh
+++ scripts/regenerate_mocks.sh
@@ -7,7 +7,7 @@ fi
 
 go install github.com/vektra/mockery/v2@latest
 
-mockery -r --output ./internal/mocks --case camel --name BinaryChecker
-mockery -r --output ./internal/mocks --case camel --name PluginBinaryDownloader
-mockery -r --output ./internal/mocks --case camel --name ProcessChecker
-mockery -r --output ./internal/mocks --case camel --name Prompter
+mockery -r --output ./internal/mocks --name BinaryChecker --filename binaryChecker.go
+mockery -r --output ./internal/mocks --name PluginBinaryDownloader --filename pluginBinaryDownloader.go
+mockery -r --output ./internal/mocks --name ProcessChecker --filename processChecker.go
+mockery -r --output ./internal/mocks --name Prompter --filename prompter.go
