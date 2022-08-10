144b6f18e9bd2dee8f15230352c8027d5fa776cb
Fabio Barone <holistic.computing@gmail.com>
2022-08-10 12:35:46 -0500

    add flags so that no prompting is possible

--- cmd/subnetcmd/join.go
+++ cmd/subnetcmd/join.go
@@ -29,6 +29,12 @@ var (
 	pluginDir string
 	// if true, print the manual instructions to screen
 	printManual bool
+	// network
+	networkStr string
+	// skipWhitelistCheck if true doesn't prompt
+	skipWhitelistCheck bool
+	// if true, doesn't ask for overwriting the config file
+	forceWrite bool
 )
 
 // avalanche subnet deploy
@@ -58,7 +64,10 @@ This command currently only supports subnets deployed on the Fuji testnet.`,
 	}
 	cmd.Flags().StringVar(&avagoConfigPath, "avalanchego-config", "", "file path of the avalanchego config file")
 	cmd.Flags().StringVar(&pluginDir, "plugin-dir", "", "file path of avalanchego's plugin directory")
+	cmd.Flags().StringVar(&networkStr, "network", "", "avalanche network where the validator will be joining (either `fuji` or `mainnet`)")
 	cmd.Flags().BoolVar(&printManual, "print", false, "if true, print the manual config without prompting")
+	cmd.Flags().BoolVar(&skipWhitelistCheck, "skip-whitelist-check", false, "if true, skip the whitelist check prompting")
+	cmd.Flags().BoolVar(&forceWrite, "force-write", false, "if true, skip to prompt to overwrite the config file")
 	return cmd
 }
 
@@ -79,12 +88,21 @@ func joinCmd(cmd *cobra.Command, args []string) error {
 	}
 
 	var network models.Network
-	networkStr, err := app.Prompt.CaptureList(
-		"Choose a network to validate on (this command only supports public networks)",
-		[]string{models.Fuji.String(), models.Mainnet.String()},
-	)
-	if err != nil {
-		return err
+	if networkStr == "" {
+		networkStr, err = app.Prompt.CaptureList(
+			"Choose a network to validate on (this command only supports public networks)",
+			[]string{models.Fuji.String(), models.Mainnet.String()},
+		)
+		if err != nil {
+			return err
+		}
+	} else {
+		// flag provided
+		networkStr = strings.Title(networkStr)
+		// as we are allowing a flag, we need to check if a supported network has been provided
+		if !(networkStr == models.Fuji.String() || networkStr == models.Mainnet.String()) {
+			return errors.New("unsupported network")
+		}
 	}
 	network = models.NetworkFromString(networkStr)
 	networkLower := strings.ToLower(network.String())
@@ -95,32 +113,34 @@ func joinCmd(cmd *cobra.Command, args []string) error {
 	}
 	subnetIDStr := subnetID.String()
 
-	ask := "Would you like to check if your node is allowed to join this subnet?\n" +
-		"If not, the subnet's control key holder must call avalanche subnet\n" +
-		"addValidator with your NodeID."
-	ux.Logger.PrintToUser(ask)
-	yes, err := app.Prompt.CaptureYesNo("Check whitelist?")
-	if err != nil {
-		return err
-	}
-	if yes {
-		isValidating, err := isNodeValidatingSubnet(subnetID, network)
+	if !skipWhitelistCheck {
+		ask := "Would you like to check if your node is allowed to join this subnet?\n" +
+			"If not, the subnet's control key holder must call avalanche subnet\n" +
+			"addValidator with your NodeID."
+		ux.Logger.PrintToUser(ask)
+		yes, err := app.Prompt.CaptureYesNo("Check whitelist?")
 		if err != nil {
 			return err
 		}
-		if !isValidating {
-			ux.Logger.PrintToUser(`The node is not whitelisted to validate this subnet.
-You can continue with this command, generating a config file or printing the whitelisting configuration,
-but until the node is whitelisted, it will not be able to validate this subnet.`)
-			y, err := app.Prompt.CaptureYesNo("Do you wish to continue")
+		if yes {
+			isValidating, err := isNodeValidatingSubnet(subnetID, network)
 			if err != nil {
 				return err
 			}
-			if !y {
-				return nil
+			if !isValidating {
+				ux.Logger.PrintToUser(`The node is not whitelisted to validate this subnet.
+You can continue with this command, generating a config file or printing the whitelisting configuration,
+but until the node is whitelisted, it will not be able to validate this subnet.`)
+				y, err := app.Prompt.CaptureYesNo("Do you wish to continue")
+				if err != nil {
+					return err
+				}
+				if !y {
+					return nil
+				}
 			}
+			ux.Logger.PrintToUser("The node is already whitelisted! You are good to go.")
 		}
-		ux.Logger.PrintToUser("The node is already whitelisted! You are good to go.")
 	}
 
 	if printManual {
@@ -133,7 +153,10 @@ but until the node is whitelisted, it will not be able to validate this subnet.`
 		return nil
 	}
 
+	// if **both** flags were set, nothing special needs to be done
+	// just check the following blocks
 	if avagoConfigPath == "" && pluginDir == "" {
+		// both flags are NOT set
 		const (
 			choiceManual    = "Manual"
 			choiceAutomatic = "Automatic"
@@ -157,6 +180,8 @@ but until the node is whitelisted, it will not be able to validate this subnet.`
 	}
 
 	// if choice is automatic, we just pass through this block
+	// or, pluginDir was set but not avagoConfigPath
+	// if **both** flags were set, this will be skipped...
 	if avagoConfigPath == "" {
 		avagoConfigPath, err = app.Prompt.CaptureString("Path to your existing config file (or where it will be generated)")
 		if err != nil {
@@ -164,11 +189,14 @@ but until the node is whitelisted, it will not be able to validate this subnet.`
 		}
 	}
 
+	// ...but not this
 	avagoConfigPath, err := sanitizePath(avagoConfigPath)
 	if err != nil {
 		return err
 	}
 
+	// avagoConfigPath was set but not pluginDir
+	// if **both** flags were set, this will be skipped...
 	if pluginDir == "" {
 		pluginDir, err = app.Prompt.CaptureString("Path to your avalanchego plugin dir (likely avalanchego/build/plugins)")
 		if err != nil {
@@ -176,6 +204,7 @@ but until the node is whitelisted, it will not be able to validate this subnet.`
 		}
 	}
 
+	// ...but not this
 	pluginDir, err := sanitizePath(pluginDir)
 	if err != nil {
 		return err
@@ -188,7 +217,7 @@ but until the node is whitelisted, it will not be able to validate this subnet.`
 
 	ux.Logger.PrintToUser("VM binary written to %s", vmPath)
 
-	if err := editConfigFile(subnetIDStr, networkLower, avagoConfigPath); err != nil {
+	if err := editConfigFile(subnetIDStr, networkLower, avagoConfigPath, forceWrite); err != nil {
 		return err
 	}
 	return nil
@@ -252,17 +281,19 @@ func checkIsValidating(subnetID ids.ID, nodeID ids.NodeID, pClient platformvm.Cl
 	return false, nil
 }
 
-func editConfigFile(subnetID string, networkID string, configFile string) error {
-	warn := "This will edit your existing config file. This edit is nondestructive,\n" +
-		"but it's always good to have a backup."
-	ux.Logger.PrintToUser(warn)
-	yes, err := app.Prompt.CaptureYesNo("Proceed?")
-	if err != nil {
-		return err
-	}
-	if !yes {
-		ux.Logger.PrintToUser("Canceled by user")
-		return nil
+func editConfigFile(subnetID string, networkID string, configFile string, forceWrite bool) error {
+	if !forceWrite {
+		warn := "This will edit your existing config file. This edit is nondestructive,\n" +
+			"but it's always good to have a backup."
+		ux.Logger.PrintToUser(warn)
+		yes, err := app.Prompt.CaptureYesNo("Proceed?")
+		if err != nil {
+			return err
+		}
+		if !yes {
+			ux.Logger.PrintToUser("Canceled by user")
+			return nil
+		}
 	}
 	fileBytes, err := os.ReadFile(configFile)
 	if err != nil && !errors.Is(err, os.ErrNotExist) {
