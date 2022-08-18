b3d7bc72008a23298381e078079b7202ea3ba9a3
Felipe Madero <felipe.madero@gmail.com>
2022-08-18 10:36:32 -0300

    add missing e2e file

---
+++ tests/e2e/subnet/local/suite.go
@@ -0,0 +1,106 @@
+// Copyright (C) 2022, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package subnet
+
+import (
+	"fmt"
+
+	"github.com/ava-labs/avalanche-cli/tests/e2e/commands"
+	"github.com/ava-labs/avalanche-cli/tests/e2e/utils"
+	ginkgo "github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+)
+
+const (
+	subnetName       = "e2eSubnetTest"
+	secondSubnetName = "e2eSecondSubnetTest"
+	genesisPath      = "tests/e2e/assets/test_genesis.json"
+)
+
+var _ = ginkgo.Describe("[Local Subnet]", func() {
+	ginkgo.AfterEach(func() {
+		commands.CleanNetwork()
+		err := utils.DeleteConfigs(subnetName)
+		gomega.Expect(err).Should(gomega.BeNil())
+		err = utils.DeleteConfigs(secondSubnetName)
+		gomega.Expect(err).Should(gomega.BeNil())
+	})
+
+	ginkgo.It("can deploy a subnet to local", func() {
+		commands.CreateSubnetConfig(subnetName, genesisPath)
+		deployOutput := commands.DeploySubnetLocally(subnetName)
+		rpcs, err := utils.ParseRPCsFromOutput(deployOutput)
+		if err != nil {
+			fmt.Println(deployOutput)
+		}
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(rpcs).Should(gomega.HaveLen(1))
+		rpc := rpcs[0]
+
+		err = utils.SetHardhatRPC(rpc)
+		gomega.Expect(err).Should(gomega.BeNil())
+
+		err = utils.RunHardhatTests(utils.BaseTest)
+		gomega.Expect(err).Should(gomega.BeNil())
+
+		commands.DeleteSubnetConfig(subnetName)
+	})
+
+	ginkgo.It("can't deploy the same subnet twice to local", func() {
+		commands.CreateSubnetConfig(subnetName, genesisPath)
+
+		deployOutput := commands.DeploySubnetLocally(subnetName)
+		rpcs, err := utils.ParseRPCsFromOutput(deployOutput)
+		if err != nil {
+			fmt.Println(deployOutput)
+		}
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(rpcs).Should(gomega.HaveLen(1))
+
+		deployOutput = commands.DeploySubnetLocally(subnetName)
+		rpcs, err = utils.ParseRPCsFromOutput(deployOutput)
+		if err == nil {
+			fmt.Println(deployOutput)
+		}
+		gomega.Expect(err).Should(gomega.HaveOccurred())
+		gomega.Expect(rpcs).Should(gomega.HaveLen(0))
+		gomega.Expect(deployOutput).Should(gomega.ContainSubstring("has already been deployed"))
+	})
+
+	ginkgo.It("can deploy multiple subnets to local", func() {
+		commands.CreateSubnetConfig(subnetName, genesisPath)
+		commands.CreateSubnetConfig(secondSubnetName, genesisPath)
+
+		deployOutput := commands.DeploySubnetLocally(subnetName)
+		rpcs, err := utils.ParseRPCsFromOutput(deployOutput)
+		if err != nil {
+			fmt.Println(deployOutput)
+		}
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(rpcs).Should(gomega.HaveLen(1))
+
+		deployOutput = commands.DeploySubnetLocally(secondSubnetName)
+		rpcs, err = utils.ParseRPCsFromOutput(deployOutput)
+		if err != nil {
+			fmt.Println(deployOutput)
+		}
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(rpcs).Should(gomega.HaveLen(2))
+
+		err = utils.SetHardhatRPC(rpcs[0])
+		gomega.Expect(err).Should(gomega.BeNil())
+
+		err = utils.RunHardhatTests(utils.BaseTest)
+		gomega.Expect(err).Should(gomega.BeNil())
+
+		err = utils.SetHardhatRPC(rpcs[1])
+		gomega.Expect(err).Should(gomega.BeNil())
+
+		err = utils.RunHardhatTests(utils.BaseTest)
+		gomega.Expect(err).Should(gomega.BeNil())
+
+		commands.DeleteSubnetConfig(subnetName)
+		commands.DeleteSubnetConfig(secondSubnetName)
+	})
+})
