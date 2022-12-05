a3e13f461a6190a90723abd495b1ed7fac47f07e
Fabio Barone <holistic.computing@gmail.com>
2022-12-05 11:08:33 -0500

    better long desc

--- cmd/subnetcmd/import.go
+++ cmd/subnetcmd/import.go
@@ -13,8 +13,11 @@ func newImportCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "import",
 		Short: "Import subnets into avalanche-cli",
-		Long: `The subnet command suite provides a collection of tools for developing,
-manage your subnet configurations and live deployments.`,
+		Long: `Import subnet configurations into avalanche-cli.
+
+This command supports importing from a file created on another computer,
+or importing from subnets running public networks
+(e.g. created manually or with the deprecated subnet-cli)`,
 		Run: func(cmd *cobra.Command, args []string) {
 			err := cmd.Help()
 			if err != nil {
