a92381ff26dc9811ec7399459a5b2861bac3a1bc
sukantoraymond <rsukanto@umich.edu>
2023-03-07 10:54:34 -0500

    directory only for migration

--- internal/migrations/subnetEVMRename.go
+++ internal/migrations/subnetEVMRename.go
@@ -4,42 +4,50 @@
 package migrations
 
 import (
+	"github.com/ava-labs/avalanche-cli/pkg/models"
 	"os"
 	"path/filepath"
 
 	"github.com/ava-labs/avalanche-cli/pkg/application"
-	"github.com/ava-labs/avalanche-cli/pkg/models"
 )
 
 const oldSubnetEVM = "SubnetEVM"
 
 func migrateSubnetEVMNames(app *application.Avalanche, runner *migrationRunner) error {
-	subnetDir := app.GetSubnetDir()
-	subnets, err := os.ReadDir(subnetDir)
+	subnetsDir := app.GetSubnetDir()
+	subnets, err := os.ReadDir(subnetsDir)
 	if err != nil {
 		return err
 	}
 
 	for _, subnet := range subnets {
-		// disregard any empty subnet directories
-		dirContents, err := os.ReadDir(filepath.Join(subnetDir, subnet.Name()))
-		if err != nil {
-			return err
-		}
-		if len(dirContents) == 0 {
-			continue
-		}
-
-		sc, err := app.LoadSidecar(subnet.Name())
+		subnetDir := filepath.Join(subnetsDir, subnet.Name())
+		fileInfo, err := os.Stat(subnetDir)
 		if err != nil {
 			return err
 		}
 
-		if string(sc.VM) == oldSubnetEVM {
-			runner.printMigrationMessage()
-			sc.VM = models.SubnetEvm
-			if err = app.UpdateSidecar(&sc); err != nil {
+		if fileInfo.IsDir() {
+			// disregard any empty subnet directories
+			dirContents, err := os.ReadDir(subnetDir)
+			if err != nil {
 				return err
+			}
+			if len(dirContents) == 0 {
+				continue
+			}
+
+			sc, err := app.LoadSidecar(subnet.Name())
+			if err != nil {
+				return err
+			}
+
+			if string(sc.VM) == oldSubnetEVM {
+				runner.printMigrationMessage()
+				sc.VM = models.SubnetEvm
+				if err = app.UpdateSidecar(&sc); err != nil {
+					return err
+				}
 			}
 		}
 	}
