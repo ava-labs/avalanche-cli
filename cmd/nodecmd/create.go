4ca709c17faac2ad54a3574698d6f086bdd55aa8
sukantoraymond <rsukanto@umich.edu>
2023-07-28 14:09:05 -0400

    move terraform to own package

--- cmd/nodecmd/create.go
+++ cmd/nodecmd/create.go
@@ -17,15 +17,14 @@ import (
 
 	"github.com/ava-labs/avalanche-cli/pkg/models"
 
+	terraform "github.com/ava-labs/avalanche-cli/pkg/terraform"
 	"github.com/ava-labs/avalanche-cli/pkg/ux"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclwrite"
 	"github.com/spf13/cobra"
-	"github.com/zclconf/go-cty/cty"
 )
 
 func newCreateCmd() *cobra.Command {
@@ -67,38 +66,6 @@ func getNewKeyPairName(ec2Svc *ec2.EC2) (string, error) {
 		}
 		ux.Logger.PrintToUser(fmt.Sprintf("Key Pair named %s already exists", newKeyPairName))
 	}
-}
-
-func removeExistingTerraformFiles() error {
-	nodeConfigFile := "node_config.tf"
-	terraformLockFile := ".terraform.lock.hcl"
-	terraformStateFile := "terraform.tfstate"
-	terraformStateBackupFile := "terraform.tfstate.backup"
-	if _, err := os.Stat(nodeConfigFile); err == nil {
-		err := os.Remove(nodeConfigFile)
-		if err != nil {
-			return err
-		}
-	}
-	if _, err := os.Stat(terraformLockFile); err == nil {
-		err := os.Remove(terraformLockFile)
-		if err != nil {
-			return err
-		}
-	}
-	if _, err := os.Stat(terraformStateFile); err == nil {
-		err := os.Remove(terraformStateFile)
-		if err != nil {
-			return err
-		}
-	}
-	if _, err := os.Stat(terraformStateBackupFile); err == nil {
-		err := os.Remove(terraformStateBackupFile)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
 }
 
 func getCertFilePath(certName string) (string, error) {
@@ -169,11 +136,14 @@ func printNoCredentialsOutput() {
 func createNode(_ *cobra.Command, args []string) error {
 	clusterName := args[0]
 
-	err := removeExistingTerraformFiles()
+	err := terraform.RemoveExistingTerraformFiles()
 	if err != nil {
 		return err
 	}
-	usr, _ := user.Current()
+	usr, err := user.Current()
+	if err != nil {
+		return err
+	}
 	keyPairName := usr.Username + "-avalanche-cli"
 	certName := keyPairName + "-kp.pem"
 	securityGroupName := keyPairName + "-sg"
@@ -192,7 +162,7 @@ func createNode(_ *cobra.Command, args []string) error {
 		printNoCredentialsOutput()
 		return err
 	}
-	err = setCloudCredentials(rootBody, credValue.AccessKeyID, credValue.SecretAccessKey, region)
+	err = terraform.SetCloudCredentials(rootBody, credValue.AccessKeyID, credValue.SecretAccessKey, region)
 	if err != nil {
 		return err
 	}
@@ -213,7 +183,7 @@ func createNode(_ *cobra.Command, args []string) error {
 		return err
 	}
 	if !keyPairExists {
-		setKeyPair(rootBody, keyPairName, certName)
+		terraform.SetKeyPair(rootBody, keyPairName, certName)
 	} else {
 		certFilePath, err := getCertFilePath(certName)
 		if err != nil {
@@ -228,7 +198,7 @@ func createNode(_ *cobra.Command, args []string) error {
 				return err
 			}
 			certName = keyPairName + "-kp.pem"
-			setKeyPair(rootBody, keyPairName, certName)
+			terraform.SetKeyPair(rootBody, keyPairName, certName)
 		}
 	}
 	securityGroupExists, sg, err := checkSecurityGroupExists(ec2Svc, securityGroupName)
@@ -240,19 +210,19 @@ func createNode(_ *cobra.Command, args []string) error {
 		return err
 	}
 	if !securityGroupExists {
-		setSecurityGroup(rootBody, userIPAddress, securityGroupName)
+		terraform.SetSecurityGroup(rootBody, userIPAddress, securityGroupName)
 	} else {
 		ipInTCP, ipInHTTP := checkCurrentIPInSg(sg, userIPAddress)
-		setSecurityGroupRule(rootBody, userIPAddress, *sg.GroupId, ipInTCP, ipInHTTP)
+		terraform.SetSecurityGroupRule(rootBody, userIPAddress, *sg.GroupId, ipInTCP, ipInHTTP)
 	}
-	setElasticIP(rootBody)
-	setUpInstance(rootBody, securityGroupName, useExistingKeyPair, keyPairName, ami)
-	setOutput(rootBody)
+	terraform.SetElasticIP(rootBody)
+	terraform.SetUpInstance(rootBody, securityGroupName, useExistingKeyPair, keyPairName, ami)
+	terraform.SetOutput(rootBody)
 	_, err = tfFile.Write(hclFile.Bytes())
 	if err != nil {
 		return err
 	}
-	instanceID, elasticIP, err := runTerraform()
+	instanceID, elasticIP, err := terraform.RunTerraform()
 	if err != nil {
 		return err
 	}
@@ -282,6 +252,10 @@ func createNode(_ *cobra.Command, args []string) error {
 		return err
 	}
 	PrintResults(instanceID, elasticIP, certFilePath, region)
+	err = terraform.RemoveExistingTerraformFiles()
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -345,53 +319,6 @@ func checkCurrentIPInSg(sg *ec2.SecurityGroup, currentIP string) (bool, bool) {
 	return ipInTCP, ipInHTTP
 }
 
-func setCloudCredentials(rootBody *hclwrite.Body, accessKey, secretKey, region string) error {
-	provider := rootBody.AppendNewBlock("provider", []string{"aws"})
-	providerBody := provider.Body()
-	providerBody.SetAttributeValue("access_key", cty.StringVal(accessKey))
-	providerBody.SetAttributeValue("secret_key", cty.StringVal(secretKey))
-	providerBody.SetAttributeValue("region", cty.StringVal(region))
-
-	return nil
-}
-
-func setKeyPair(rootBody *hclwrite.Body, keyName, certName string) {
-	tlsPrivateKey := rootBody.AppendNewBlock("resource", []string{"tls_private_key", "pk"})
-	tlsPrivateKeyBody := tlsPrivateKey.Body()
-	tlsPrivateKeyBody.SetAttributeValue("algorithm", cty.StringVal("RSA"))
-	tlsPrivateKeyBody.SetAttributeValue("rsa_bits", cty.NumberIntVal(4096))
-
-	keyPair := rootBody.AppendNewBlock("resource", []string{"aws_key_pair", "kp"})
-	keyPairBody := keyPair.Body()
-	keyPairBody.SetAttributeValue("key_name", cty.StringVal(keyName))
-	keyPairBody.SetAttributeTraversal("public_key", hcl.Traversal{
-		hcl.TraverseRoot{
-			Name: "tls_private_key",
-		},
-		hcl.TraverseAttr{
-			Name: "pk",
-		},
-		hcl.TraverseAttr{
-			Name: "public_key_openssh",
-		},
-	})
-
-	tfKey := rootBody.AppendNewBlock("resource", []string{"local_file", "tf-key"})
-	tfKeyBody := tfKey.Body()
-	tfKeyBody.SetAttributeValue("filename", cty.StringVal(certName))
-	tfKeyBody.SetAttributeTraversal("content", hcl.Traversal{
-		hcl.TraverseRoot{
-			Name: "tls_private_key",
-		},
-		hcl.TraverseAttr{
-			Name: "pk",
-		},
-		hcl.TraverseAttr{
-			Name: "private_key_pem",
-		},
-	})
-}
-
 func getIPAddress() (string, error) {
 	ipOutput, err := exec.Command("curl", "ipecho.net/plain").Output()
 	if err != nil {
@@ -402,181 +329,6 @@ func getIPAddress() (string, error) {
 		return "", errors.New("invalid IP address")
 	}
 	return ipAddress, nil
-}
-
-func setSecurityGroup(rootBody *hclwrite.Body, ipAddress, securityGroupName string) {
-	inputIPAddress := ipAddress + "/32"
-	securityGroup := rootBody.AppendNewBlock("resource", []string{"aws_security_group", "ssh_avax_sg"})
-	securityGroupBody := securityGroup.Body()
-	securityGroupBody.SetAttributeValue("name", cty.StringVal(securityGroupName))
-	securityGroupBody.SetAttributeValue("description", cty.StringVal("Allow SSH, AVAX HTTP outbound traffic"))
-	inboundGroup := securityGroupBody.AppendNewBlock("ingress", []string{})
-	inboundGroupBody := inboundGroup.Body()
-	inboundGroupBody.SetAttributeValue("description", cty.StringVal("TCP"))
-	inboundGroupBody.SetAttributeValue("from_port", cty.NumberIntVal(22))
-	inboundGroupBody.SetAttributeValue("to_port", cty.NumberIntVal(22))
-	inboundGroupBody.SetAttributeValue("protocol", cty.StringVal("tcp"))
-	var ipList []cty.Value
-	ipList = append(ipList, cty.StringVal(inputIPAddress))
-	inboundGroupBody.SetAttributeValue("cidr_blocks", cty.ListVal(ipList))
-
-	inboundGroup = securityGroupBody.AppendNewBlock("ingress", []string{})
-	inboundGroupBody = inboundGroup.Body()
-	inboundGroupBody.SetAttributeValue("description", cty.StringVal("AVAX HTTP"))
-	inboundGroupBody.SetAttributeValue("from_port", cty.NumberIntVal(9650))
-	inboundGroupBody.SetAttributeValue("to_port", cty.NumberIntVal(9650))
-	inboundGroupBody.SetAttributeValue("protocol", cty.StringVal("tcp"))
-	ipList = []cty.Value{}
-	ipList = append(ipList, cty.StringVal("0.0.0.0/0"))
-	inboundGroupBody.SetAttributeValue("cidr_blocks", cty.ListVal(ipList))
-
-	inboundGroup = securityGroupBody.AppendNewBlock("ingress", []string{})
-	inboundGroupBody = inboundGroup.Body()
-	inboundGroupBody.SetAttributeValue("description", cty.StringVal("AVAX HTTP"))
-	inboundGroupBody.SetAttributeValue("from_port", cty.NumberIntVal(9650))
-	inboundGroupBody.SetAttributeValue("to_port", cty.NumberIntVal(9650))
-	inboundGroupBody.SetAttributeValue("protocol", cty.StringVal("tcp"))
-	ipList = []cty.Value{}
-	ipList = append(ipList, cty.StringVal(inputIPAddress))
-	inboundGroupBody.SetAttributeValue("cidr_blocks", cty.ListVal(ipList))
-
-	inboundGroup = securityGroupBody.AppendNewBlock("ingress", []string{})
-	inboundGroupBody = inboundGroup.Body()
-	inboundGroupBody.SetAttributeValue("description", cty.StringVal("AVAX Staking"))
-	inboundGroupBody.SetAttributeValue("from_port", cty.NumberIntVal(9651))
-	inboundGroupBody.SetAttributeValue("to_port", cty.NumberIntVal(9651))
-	inboundGroupBody.SetAttributeValue("protocol", cty.StringVal("tcp"))
-	ipList = []cty.Value{}
-	ipList = append(ipList, cty.StringVal("0.0.0.0/0"))
-	inboundGroupBody.SetAttributeValue("cidr_blocks", cty.ListVal(ipList))
-
-	inboundGroup = securityGroupBody.AppendNewBlock("egress", []string{})
-	inboundGroupBody = inboundGroup.Body()
-	inboundGroupBody.SetAttributeValue("description", cty.StringVal("Outbound traffic"))
-	inboundGroupBody.SetAttributeValue("from_port", cty.NumberIntVal(0))
-	inboundGroupBody.SetAttributeValue("to_port", cty.NumberIntVal(0))
-	inboundGroupBody.SetAttributeValue("protocol", cty.StringVal("-1"))
-	ipList = []cty.Value{}
-	ipList = append(ipList, cty.StringVal("0.0.0.0/0"))
-	inboundGroupBody.SetAttributeValue("cidr_blocks", cty.ListVal(ipList))
-}
-
-func setSecurityGroupRule(rootBody *hclwrite.Body, ipAddress, sgID string, ipInTCP, ipInHTTP bool) {
-	inputIPAddress := ipAddress + "/32"
-	if !ipInTCP {
-		sgRuleName := "ipTcp" + strings.ReplaceAll(ipAddress, ".", "")
-		securityGroupRule := rootBody.AppendNewBlock("resource", []string{"aws_security_group_rule", sgRuleName})
-		securityGroupRuleBody := securityGroupRule.Body()
-		securityGroupRuleBody.SetAttributeValue("type", cty.StringVal("ingress"))
-		securityGroupRuleBody.SetAttributeValue("from_port", cty.NumberIntVal(22))
-		securityGroupRuleBody.SetAttributeValue("to_port", cty.NumberIntVal(22))
-		securityGroupRuleBody.SetAttributeValue("protocol", cty.StringVal("tcp"))
-		var ipList []cty.Value
-		ipList = append(ipList, cty.StringVal(inputIPAddress))
-		securityGroupRuleBody.SetAttributeValue("cidr_blocks", cty.ListVal(ipList))
-		securityGroupRuleBody.SetAttributeValue("security_group_id", cty.StringVal(sgID))
-	}
-	if !ipInHTTP {
-		sgRuleName := "ipHttp" + strings.ReplaceAll(ipAddress, ".", "")
-		// sgRuleName := "ipHttp"
-		securityGroupRule := rootBody.AppendNewBlock("resource", []string{"aws_security_group_rule", sgRuleName})
-		securityGroupRuleBody := securityGroupRule.Body()
-		securityGroupRuleBody.SetAttributeValue("type", cty.StringVal("ingress"))
-		securityGroupRuleBody.SetAttributeValue("from_port", cty.NumberIntVal(9650))
-		securityGroupRuleBody.SetAttributeValue("to_port", cty.NumberIntVal(9650))
-		securityGroupRuleBody.SetAttributeValue("protocol", cty.StringVal("tcp"))
-		var ipList []cty.Value
-		ipList = append(ipList, cty.StringVal(inputIPAddress))
-		securityGroupRuleBody.SetAttributeValue("cidr_blocks", cty.ListVal(ipList))
-		securityGroupRuleBody.SetAttributeValue("security_group_id", cty.StringVal(sgID))
-	}
-}
-
-func setElasticIP(rootBody *hclwrite.Body) {
-	eip := rootBody.AppendNewBlock("resource", []string{"aws_eip", "myeip"})
-	eipBody := eip.Body()
-	eipBody.SetAttributeValue("vpc", cty.BoolVal(true))
-
-	eipAssoc := rootBody.AppendNewBlock("resource", []string{"aws_eip_association", "eip_assoc"})
-	eipAssocBody := eipAssoc.Body()
-	eipAssocBody.SetAttributeTraversal("instance_id", hcl.Traversal{
-		hcl.TraverseRoot{
-			Name: "aws_instance",
-		},
-		hcl.TraverseAttr{
-			Name: "fuji_node[0]",
-		},
-		hcl.TraverseAttr{
-			Name: "id",
-		},
-	})
-	eipAssocBody.SetAttributeTraversal("allocation_id", hcl.Traversal{
-		hcl.TraverseRoot{
-			Name: "aws_eip",
-		},
-		hcl.TraverseAttr{
-			Name: "myeip",
-		},
-		hcl.TraverseAttr{
-			Name: "id",
-		},
-	})
-}
-
-func setUpInstance(rootBody *hclwrite.Body, securityGroupName string, useExistingKeyPair bool, existingKeyPairName, ami string) {
-	awsInstance := rootBody.AppendNewBlock("resource", []string{"aws_instance", "fuji_node"})
-	awsInstanceBody := awsInstance.Body()
-	awsInstanceBody.SetAttributeValue("count", cty.NumberIntVal(1))
-	awsInstanceBody.SetAttributeValue("ami", cty.StringVal(ami))
-	awsInstanceBody.SetAttributeValue("instance_type", cty.StringVal("c5.2xlarge"))
-	if !useExistingKeyPair {
-		awsInstanceBody.SetAttributeTraversal("key_name", hcl.Traversal{
-			hcl.TraverseRoot{
-				Name: "aws_key_pair",
-			},
-			hcl.TraverseAttr{
-				Name: "kp",
-			},
-			hcl.TraverseAttr{
-				Name: "key_name",
-			},
-		})
-	} else {
-		awsInstanceBody.SetAttributeValue("key_name", cty.StringVal(existingKeyPairName))
-	}
-	var securityGroupList []cty.Value
-	securityGroupList = append(securityGroupList, cty.StringVal(securityGroupName))
-	awsInstanceBody.SetAttributeValue("security_groups", cty.ListVal(securityGroupList))
-}
-
-func setOutput(rootBody *hclwrite.Body) {
-	outputEip := rootBody.AppendNewBlock("output", []string{"instance_eip"})
-	outputEipBody := outputEip.Body()
-	outputEipBody.SetAttributeTraversal("value", hcl.Traversal{
-		hcl.TraverseRoot{
-			Name: "aws_eip",
-		},
-		hcl.TraverseAttr{
-			Name: "myeip",
-		},
-		hcl.TraverseAttr{
-			Name: "public_ip",
-		},
-	})
-
-	outputInstanceID := rootBody.AppendNewBlock("output", []string{"instance_id"})
-	outputInstanceIDBody := outputInstanceID.Body()
-	outputInstanceIDBody.SetAttributeTraversal("value", hcl.Traversal{
-		hcl.TraverseRoot{
-			Name: "aws_instance",
-		},
-		hcl.TraverseAttr{
-			Name: "fuji_node[0]",
-		},
-		hcl.TraverseAttr{
-			Name: "id",
-		},
-	})
 }
 
 func createAnsibleHostInventory(inventoryPath, elasticIP, certFilePath string) error {
@@ -600,34 +352,6 @@ func createAnsibleHostInventory(inventoryPath, elasticIP, certFilePath string) e
 		return err
 	}
 	return nil
-}
-
-func runTerraform() (string, string, error) {
-	var instanceID string
-	var elasticIP string
-	if err := exec.Command("terraform", "init").Run(); err != nil {
-		return "", "", err
-	}
-	var stdBuffer bytes.Buffer
-	cmd := exec.Command("terraform", "apply", "-auto-approve")
-	mw := io.MultiWriter(os.Stdout, &stdBuffer)
-	cmd.Stdout = mw
-	cmd.Stderr = mw
-	if err := cmd.Run(); err != nil {
-		return "", "", err
-	}
-
-	instanceIDOutput, err := exec.Command("terraform", "output", "instance_id").Output()
-	if err != nil {
-		return "", "", err
-	}
-	instanceID = string(instanceIDOutput)
-	eipOutput, err := exec.Command("terraform", "output", "instance_eip").Output()
-	if err != nil {
-		return "", "", err
-	}
-	elasticIP = string(eipOutput)
-	return instanceID, elasticIP, nil
 }
 
 func handleCerts(certName string) error {
---
+++ pkg/terraform/terraform.go
@@ -0,0 +1,297 @@
+// Copyright (C) 2022, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package terraform
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclwrite"
+	"github.com/zclconf/go-cty/cty"
+)
+
+func SetCloudCredentials(rootBody *hclwrite.Body, accessKey, secretKey, region string) error {
+	provider := rootBody.AppendNewBlock("provider", []string{"aws"})
+	providerBody := provider.Body()
+	providerBody.SetAttributeValue("access_key", cty.StringVal(accessKey))
+	providerBody.SetAttributeValue("secret_key", cty.StringVal(secretKey))
+	providerBody.SetAttributeValue("region", cty.StringVal(region))
+
+	return nil
+}
+
+func SetSecurityGroup(rootBody *hclwrite.Body, ipAddress, securityGroupName string) {
+	inputIPAddress := ipAddress + "/32"
+	securityGroup := rootBody.AppendNewBlock("resource", []string{"aws_security_group", "ssh_avax_sg"})
+	securityGroupBody := securityGroup.Body()
+	securityGroupBody.SetAttributeValue("name", cty.StringVal(securityGroupName))
+	securityGroupBody.SetAttributeValue("description", cty.StringVal("Allow SSH, AVAX HTTP outbound traffic"))
+	inboundGroup := securityGroupBody.AppendNewBlock("ingress", []string{})
+	inboundGroupBody := inboundGroup.Body()
+	inboundGroupBody.SetAttributeValue("description", cty.StringVal("TCP"))
+	inboundGroupBody.SetAttributeValue("from_port", cty.NumberIntVal(22))
+	inboundGroupBody.SetAttributeValue("to_port", cty.NumberIntVal(22))
+	inboundGroupBody.SetAttributeValue("protocol", cty.StringVal("tcp"))
+	var ipList []cty.Value
+	ipList = append(ipList, cty.StringVal(inputIPAddress))
+	inboundGroupBody.SetAttributeValue("cidr_blocks", cty.ListVal(ipList))
+
+	inboundGroup = securityGroupBody.AppendNewBlock("ingress", []string{})
+	inboundGroupBody = inboundGroup.Body()
+	inboundGroupBody.SetAttributeValue("description", cty.StringVal("AVAX HTTP"))
+	inboundGroupBody.SetAttributeValue("from_port", cty.NumberIntVal(9650))
+	inboundGroupBody.SetAttributeValue("to_port", cty.NumberIntVal(9650))
+	inboundGroupBody.SetAttributeValue("protocol", cty.StringVal("tcp"))
+	ipList = []cty.Value{}
+	ipList = append(ipList, cty.StringVal("0.0.0.0/0"))
+	inboundGroupBody.SetAttributeValue("cidr_blocks", cty.ListVal(ipList))
+
+	inboundGroup = securityGroupBody.AppendNewBlock("ingress", []string{})
+	inboundGroupBody = inboundGroup.Body()
+	inboundGroupBody.SetAttributeValue("description", cty.StringVal("AVAX HTTP"))
+	inboundGroupBody.SetAttributeValue("from_port", cty.NumberIntVal(9650))
+	inboundGroupBody.SetAttributeValue("to_port", cty.NumberIntVal(9650))
+	inboundGroupBody.SetAttributeValue("protocol", cty.StringVal("tcp"))
+	ipList = []cty.Value{}
+	ipList = append(ipList, cty.StringVal(inputIPAddress))
+	inboundGroupBody.SetAttributeValue("cidr_blocks", cty.ListVal(ipList))
+
+	inboundGroup = securityGroupBody.AppendNewBlock("ingress", []string{})
+	inboundGroupBody = inboundGroup.Body()
+	inboundGroupBody.SetAttributeValue("description", cty.StringVal("AVAX Staking"))
+	inboundGroupBody.SetAttributeValue("from_port", cty.NumberIntVal(9651))
+	inboundGroupBody.SetAttributeValue("to_port", cty.NumberIntVal(9651))
+	inboundGroupBody.SetAttributeValue("protocol", cty.StringVal("tcp"))
+	ipList = []cty.Value{}
+	ipList = append(ipList, cty.StringVal("0.0.0.0/0"))
+	inboundGroupBody.SetAttributeValue("cidr_blocks", cty.ListVal(ipList))
+
+	inboundGroup = securityGroupBody.AppendNewBlock("egress", []string{})
+	inboundGroupBody = inboundGroup.Body()
+	inboundGroupBody.SetAttributeValue("description", cty.StringVal("Outbound traffic"))
+	inboundGroupBody.SetAttributeValue("from_port", cty.NumberIntVal(0))
+	inboundGroupBody.SetAttributeValue("to_port", cty.NumberIntVal(0))
+	inboundGroupBody.SetAttributeValue("protocol", cty.StringVal("-1"))
+	ipList = []cty.Value{}
+	ipList = append(ipList, cty.StringVal("0.0.0.0/0"))
+	inboundGroupBody.SetAttributeValue("cidr_blocks", cty.ListVal(ipList))
+}
+
+func SetSecurityGroupRule(rootBody *hclwrite.Body, ipAddress, sgID string, ipInTCP, ipInHTTP bool) {
+	inputIPAddress := ipAddress + "/32"
+	if !ipInTCP {
+		sgRuleName := "ipTcp" + strings.ReplaceAll(ipAddress, ".", "")
+		securityGroupRule := rootBody.AppendNewBlock("resource", []string{"aws_security_group_rule", sgRuleName})
+		securityGroupRuleBody := securityGroupRule.Body()
+		securityGroupRuleBody.SetAttributeValue("type", cty.StringVal("ingress"))
+		securityGroupRuleBody.SetAttributeValue("from_port", cty.NumberIntVal(22))
+		securityGroupRuleBody.SetAttributeValue("to_port", cty.NumberIntVal(22))
+		securityGroupRuleBody.SetAttributeValue("protocol", cty.StringVal("tcp"))
+		var ipList []cty.Value
+		ipList = append(ipList, cty.StringVal(inputIPAddress))
+		securityGroupRuleBody.SetAttributeValue("cidr_blocks", cty.ListVal(ipList))
+		securityGroupRuleBody.SetAttributeValue("security_group_id", cty.StringVal(sgID))
+	}
+	if !ipInHTTP {
+		sgRuleName := "ipHttp" + strings.ReplaceAll(ipAddress, ".", "")
+		securityGroupRule := rootBody.AppendNewBlock("resource", []string{"aws_security_group_rule", sgRuleName})
+		securityGroupRuleBody := securityGroupRule.Body()
+		securityGroupRuleBody.SetAttributeValue("type", cty.StringVal("ingress"))
+		securityGroupRuleBody.SetAttributeValue("from_port", cty.NumberIntVal(9650))
+		securityGroupRuleBody.SetAttributeValue("to_port", cty.NumberIntVal(9650))
+		securityGroupRuleBody.SetAttributeValue("protocol", cty.StringVal("tcp"))
+		var ipList []cty.Value
+		ipList = append(ipList, cty.StringVal(inputIPAddress))
+		securityGroupRuleBody.SetAttributeValue("cidr_blocks", cty.ListVal(ipList))
+		securityGroupRuleBody.SetAttributeValue("security_group_id", cty.StringVal(sgID))
+	}
+}
+
+func SetElasticIP(rootBody *hclwrite.Body) {
+	eip := rootBody.AppendNewBlock("resource", []string{"aws_eip", "myeip"})
+	eipBody := eip.Body()
+	eipBody.SetAttributeValue("vpc", cty.BoolVal(true))
+
+	eipAssoc := rootBody.AppendNewBlock("resource", []string{"aws_eip_association", "eip_assoc"})
+	eipAssocBody := eipAssoc.Body()
+	eipAssocBody.SetAttributeTraversal("instance_id", hcl.Traversal{
+		hcl.TraverseRoot{
+			Name: "aws_instance",
+		},
+		hcl.TraverseAttr{
+			Name: "fuji_node[0]",
+		},
+		hcl.TraverseAttr{
+			Name: "id",
+		},
+	})
+	eipAssocBody.SetAttributeTraversal("allocation_id", hcl.Traversal{
+		hcl.TraverseRoot{
+			Name: "aws_eip",
+		},
+		hcl.TraverseAttr{
+			Name: "myeip",
+		},
+		hcl.TraverseAttr{
+			Name: "id",
+		},
+	})
+}
+
+func SetKeyPair(rootBody *hclwrite.Body, keyName, certName string) {
+	tlsPrivateKey := rootBody.AppendNewBlock("resource", []string{"tls_private_key", "pk"})
+	tlsPrivateKeyBody := tlsPrivateKey.Body()
+	tlsPrivateKeyBody.SetAttributeValue("algorithm", cty.StringVal("RSA"))
+	tlsPrivateKeyBody.SetAttributeValue("rsa_bits", cty.NumberIntVal(4096))
+
+	keyPair := rootBody.AppendNewBlock("resource", []string{"aws_key_pair", "kp"})
+	keyPairBody := keyPair.Body()
+	keyPairBody.SetAttributeValue("key_name", cty.StringVal(keyName))
+	keyPairBody.SetAttributeTraversal("public_key", hcl.Traversal{
+		hcl.TraverseRoot{
+			Name: "tls_private_key",
+		},
+		hcl.TraverseAttr{
+			Name: "pk",
+		},
+		hcl.TraverseAttr{
+			Name: "public_key_openssh",
+		},
+	})
+
+	tfKey := rootBody.AppendNewBlock("resource", []string{"local_file", "tf-key"})
+	tfKeyBody := tfKey.Body()
+	tfKeyBody.SetAttributeValue("filename", cty.StringVal(certName))
+	tfKeyBody.SetAttributeTraversal("content", hcl.Traversal{
+		hcl.TraverseRoot{
+			Name: "tls_private_key",
+		},
+		hcl.TraverseAttr{
+			Name: "pk",
+		},
+		hcl.TraverseAttr{
+			Name: "private_key_pem",
+		},
+	})
+}
+
+func SetUpInstance(rootBody *hclwrite.Body, securityGroupName string, useExistingKeyPair bool, existingKeyPairName, ami string) {
+	awsInstance := rootBody.AppendNewBlock("resource", []string{"aws_instance", "fuji_node"})
+	awsInstanceBody := awsInstance.Body()
+	awsInstanceBody.SetAttributeValue("count", cty.NumberIntVal(1))
+	awsInstanceBody.SetAttributeValue("ami", cty.StringVal(ami))
+	awsInstanceBody.SetAttributeValue("instance_type", cty.StringVal("c5.2xlarge"))
+	if !useExistingKeyPair {
+		awsInstanceBody.SetAttributeTraversal("key_name", hcl.Traversal{
+			hcl.TraverseRoot{
+				Name: "aws_key_pair",
+			},
+			hcl.TraverseAttr{
+				Name: "kp",
+			},
+			hcl.TraverseAttr{
+				Name: "key_name",
+			},
+		})
+	} else {
+		awsInstanceBody.SetAttributeValue("key_name", cty.StringVal(existingKeyPairName))
+	}
+	var securityGroupList []cty.Value
+	securityGroupList = append(securityGroupList, cty.StringVal(securityGroupName))
+	awsInstanceBody.SetAttributeValue("security_groups", cty.ListVal(securityGroupList))
+}
+
+func SetOutput(rootBody *hclwrite.Body) {
+	outputEip := rootBody.AppendNewBlock("output", []string{"instance_eip"})
+	outputEipBody := outputEip.Body()
+	outputEipBody.SetAttributeTraversal("value", hcl.Traversal{
+		hcl.TraverseRoot{
+			Name: "aws_eip",
+		},
+		hcl.TraverseAttr{
+			Name: "myeip",
+		},
+		hcl.TraverseAttr{
+			Name: "public_ip",
+		},
+	})
+
+	outputInstanceID := rootBody.AppendNewBlock("output", []string{"instance_id"})
+	outputInstanceIDBody := outputInstanceID.Body()
+	outputInstanceIDBody.SetAttributeTraversal("value", hcl.Traversal{
+		hcl.TraverseRoot{
+			Name: "aws_instance",
+		},
+		hcl.TraverseAttr{
+			Name: "fuji_node[0]",
+		},
+		hcl.TraverseAttr{
+			Name: "id",
+		},
+	})
+}
+
+func RemoveExistingTerraformFiles() error {
+	nodeConfigFile := "node_config.tf"
+	terraformLockFile := ".terraform.lock.hcl"
+	terraformStateFile := "terraform.tfstate"
+	terraformStateBackupFile := "terraform.tfstate.backup"
+	if _, err := os.Stat(nodeConfigFile); err == nil {
+		err := os.Remove(nodeConfigFile)
+		if err != nil {
+			return err
+		}
+	}
+	if _, err := os.Stat(terraformLockFile); err == nil {
+		err := os.Remove(terraformLockFile)
+		if err != nil {
+			return err
+		}
+	}
+	if _, err := os.Stat(terraformStateFile); err == nil {
+		err := os.Remove(terraformStateFile)
+		if err != nil {
+			return err
+		}
+	}
+	if _, err := os.Stat(terraformStateBackupFile); err == nil {
+		err := os.Remove(terraformStateBackupFile)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func RunTerraform() (string, string, error) {
+	var instanceID string
+	var elasticIP string
+	if err := exec.Command("terraform", "init").Run(); err != nil {
+		return "", "", err
+	}
+	var stdBuffer bytes.Buffer
+	cmd := exec.Command("terraform", "apply", "-auto-approve")
+	mw := io.MultiWriter(os.Stdout, &stdBuffer)
+	cmd.Stdout = mw
+	cmd.Stderr = mw
+	if err := cmd.Run(); err != nil {
+		return "", "", err
+	}
+
+	instanceIDOutput, err := exec.Command("terraform", "output", "instance_id").Output()
+	if err != nil {
+		return "", "", err
+	}
+	instanceID = string(instanceIDOutput)
+	eipOutput, err := exec.Command("terraform", "output", "instance_eip").Output()
+	if err != nil {
+		return "", "", err
+	}
+	elasticIP = string(eipOutput)
+	return instanceID, elasticIP, nil
+}
