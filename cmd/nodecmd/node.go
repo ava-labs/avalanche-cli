20465142288ba676ae71b882e9fa3eb145ac47fd
sukantoraymond <rsukanto@umich.edu>
2023-08-14 15:04:39 -0400

    no op

--- cmd/nodecmd/node.go
+++ cmd/nodecmd/node.go
@@ -38,6 +38,5 @@ rest of the commands to maintain your node and make your node a Subnet Validator
 	cmd.AddCommand(newSyncCmd())
 	// node status cluster
 	cmd.AddCommand(newStatusCmd())
-
 	return cmd
 }
