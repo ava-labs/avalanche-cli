6888f100707d18536e586b3095e5a540fb555f14
sukantoraymond <rsukanto@umich.edu>
2023-09-07 18:12:26 -0400

    move command

--- cmd/nodecmd/node.go
+++ cmd/nodecmd/node.go
@@ -42,5 +42,7 @@ rest of the commands to maintain your node and make your node a Subnet Validator
 	cmd.AddCommand(newStatusCmd())
 	// node list
 	cmd.AddCommand(newListCmd())
+	// node move
+	cmd.AddCommand(newMoveCmd())
 	return cmd
 }
