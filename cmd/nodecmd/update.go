08d0a7a4d163706c7af3a7f524a7f4fd67d146c4
fm <felipe.madero@gmail.com>
2023-09-11 18:55:59 -0300

    new update subnet command

--- cmd/nodecmd/update.go
+++ cmd/nodecmd/update.go
@@ -3,82 +3,28 @@
 package nodecmd
 
 import (
-	"errors"
 	"fmt"
 
-	"github.com/ava-labs/avalanche-cli/pkg/constants"
-
-	"github.com/ava-labs/avalanche-cli/pkg/ansible"
-
-	"github.com/ava-labs/avalanche-cli/cmd/subnetcmd"
-	"github.com/ava-labs/avalanche-cli/pkg/models"
-	"github.com/ava-labs/avalanche-cli/pkg/ux"
 	"github.com/spf13/cobra"
 )
 
 func newUpdateCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "update [clusterName] [subnetName]",
-		Short: "(ALPHA Warning) Update nodes in a cluster with latest subnet configuration and virtual machine",
+		Use:   "update",
+		Short: "(ALPHA Warning) Update avalanchego or VM version/config for all node in a cluster",
 		Long: `(ALPHA Warning) This command is currently in experimental mode.
 
-The node update command updates all nodes in a cluster with latest Subnet configuration and virtual machine.
-You can check the updated subnet bootstrap status by calling avalanche node status <clusterName> --subnet <subnetName>`,
-		SilenceUsage: true,
-		Args:         cobra.ExactArgs(2),
-		RunE:         updateSubnet,
+The node update command suite provides a collection of commands for nodes to update
+their avalanchego version or VM version/config.
+You can check the status after update by calling avalanche node status`,
+		Run: func(cmd *cobra.Command, args []string) {
+			err := cmd.Help()
+			if err != nil {
+				fmt.Println(err)
+			}
+		},
 	}
-
+	// node update subnet
+	cmd.AddCommand(newUpdateSubnetCmd())
 	return cmd
-}
-
-func updateSubnet(_ *cobra.Command, args []string) error {
-	clusterName := args[0]
-	subnetName := args[1]
-	if err := checkCluster(clusterName); err != nil {
-		return err
-	}
-	if err := setupAnsible(); err != nil {
-		return err
-	}
-	if _, err := subnetcmd.ValidateSubnetNameAndGetChains([]string{subnetName}); err != nil {
-		return err
-	}
-	isBootstrapped, err := checkNodeIsBootstrapped(clusterName, false)
-	if err != nil {
-		return err
-	}
-	if !isBootstrapped {
-		return errors.New("node is not bootstrapped yet, please try again later")
-	}
-	if err := checkAvalancheGoVersionCompatible(clusterName, subnetName); err != nil {
-		return err
-	}
-	ux.Logger.PrintToUser("Installing Custom VM build environment on the EC2 instance ...")
-	if err := ansible.RunAnsiblePlaybookSetupBuildEnv(app.GetAnsibleDir(), app.GetAnsibleInventoryPath(clusterName)); err != nil {
-		return err
-	}
-	if err := ansible.RunAnsiblePlaybookSetupCLIFromSource(app.GetAnsibleDir(), app.GetAnsibleInventoryPath(clusterName), constants.CloudCLIBranch); err != nil {
-		return err
-	}
-	return doUpdateSubnet(clusterName, subnetName, models.Fuji)
-}
-
-// doUpdateSubnet exports deployed subnet in user's local machine to cloud server and calls node to
-// restart tracking the specified subnet (similar to avalanche subnet join <subnetName> command)
-func doUpdateSubnet(clusterName, subnetName string, network models.Network) error {
-	subnetPath := "/tmp/" + subnetName + constants.ExportSubnetSuffix
-	if err := subnetcmd.CallExportSubnet(subnetName, subnetPath, network); err != nil {
-		return err
-	}
-	if err := ansible.RunAnsiblePlaybookExportSubnet(app.GetAnsibleDir(), app.GetAnsibleInventoryPath(clusterName), subnetPath, "/tmp"); err != nil {
-		return err
-	}
-	// runs avalanche join subnet command
-	if err := ansible.RunAnsiblePlaybookUpdateSubnet(app.GetAnsibleDir(), subnetName, subnetPath, app.GetAnsibleInventoryPath(clusterName)); err != nil {
-		return err
-	}
-	ux.Logger.PrintToUser("Node successfully updated Subnet!")
-	ux.Logger.PrintToUser(fmt.Sprintf("Check node subnet resyncing status with avalanche node status %s --subnet %s", clusterName, subnetName))
-	return nil
 }
---
+++ cmd/nodecmd/updateSubnet.go
@@ -0,0 +1,84 @@
+// Copyright (C) 2022, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+package nodecmd
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/ava-labs/avalanche-cli/pkg/constants"
+
+	"github.com/ava-labs/avalanche-cli/pkg/ansible"
+
+	"github.com/ava-labs/avalanche-cli/cmd/subnetcmd"
+	"github.com/ava-labs/avalanche-cli/pkg/models"
+	"github.com/ava-labs/avalanche-cli/pkg/ux"
+	"github.com/spf13/cobra"
+)
+
+func newUpdateSubnetCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "subnet [clusterName] [subnetName]",
+		Short: "(ALPHA Warning) Update nodes in a cluster with latest subnet configuration and virtual machine",
+		Long: `(ALPHA Warning) This command is currently in experimental mode.
+
+The node update subnet command updates all nodes in a cluster with latest Subnet configuration and virtual machine.
+You can check the updated subnet bootstrap status by calling avalanche node status <clusterName> --subnet <subnetName>`,
+		SilenceUsage: true,
+		Args:         cobra.ExactArgs(2),
+		RunE:         updateSubnet,
+	}
+
+	return cmd
+}
+
+func updateSubnet(_ *cobra.Command, args []string) error {
+	clusterName := args[0]
+	subnetName := args[1]
+	if err := checkCluster(clusterName); err != nil {
+		return err
+	}
+	if err := setupAnsible(); err != nil {
+		return err
+	}
+	if _, err := subnetcmd.ValidateSubnetNameAndGetChains([]string{subnetName}); err != nil {
+		return err
+	}
+	isBootstrapped, err := checkNodeIsBootstrapped(clusterName, false)
+	if err != nil {
+		return err
+	}
+	if !isBootstrapped {
+		return errors.New("node is not bootstrapped yet, please try again later")
+	}
+	if err := checkAvalancheGoVersionCompatible(clusterName, subnetName); err != nil {
+		return err
+	}
+	ux.Logger.PrintToUser("Installing Custom VM build environment on the EC2 instance ...")
+	if err := ansible.RunAnsiblePlaybookSetupBuildEnv(app.GetAnsibleDir(), app.GetAnsibleInventoryPath(clusterName)); err != nil {
+		return err
+	}
+	if err := ansible.RunAnsiblePlaybookSetupCLIFromSource(app.GetAnsibleDir(), app.GetAnsibleInventoryPath(clusterName), constants.CloudCLIBranch); err != nil {
+		return err
+	}
+	return doUpdateSubnet(clusterName, subnetName, models.Fuji)
+}
+
+// doUpdateSubnet exports deployed subnet in user's local machine to cloud server and calls node to
+// restart tracking the specified subnet (similar to avalanche subnet join <subnetName> command)
+func doUpdateSubnet(clusterName, subnetName string, network models.Network) error {
+	subnetPath := "/tmp/" + subnetName + constants.ExportSubnetSuffix
+	if err := subnetcmd.CallExportSubnet(subnetName, subnetPath, network); err != nil {
+		return err
+	}
+	if err := ansible.RunAnsiblePlaybookExportSubnet(app.GetAnsibleDir(), app.GetAnsibleInventoryPath(clusterName), subnetPath, "/tmp"); err != nil {
+		return err
+	}
+	// runs avalanche join subnet command
+	if err := ansible.RunAnsiblePlaybookUpdateSubnet(app.GetAnsibleDir(), subnetName, subnetPath, app.GetAnsibleInventoryPath(clusterName)); err != nil {
+		return err
+	}
+	ux.Logger.PrintToUser("Node successfully updated Subnet!")
+	ux.Logger.PrintToUser(fmt.Sprintf("Check node subnet resyncing status with avalanche node status %s --subnet %s", clusterName, subnetName))
+	return nil
+}
