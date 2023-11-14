dd481bbffc5071408a5b33ecef942a04d76812f9
Felipe Madero <felipe.madero@gmail.com>
2023-11-14 16:46:49 -0300

    add create node to wiz

--- cmd/nodecmd/wiz.go
+++ cmd/nodecmd/wiz.go
@@ -3,6 +3,7 @@
 package nodecmd
 
 import (
+	"github.com/ava-labs/avalanche-cli/pkg/constants"
 	"github.com/spf13/cobra"
 )
 
@@ -18,13 +19,25 @@ The node wiz command creates a devnet and deploys, sync and validate a subnet in
 		Args:         cobra.ExactArgs(2),
 		RunE:         wiz,
 	}
+	cmd.Flags().BoolVar(&useStaticIP, "use-static-ip", true, "attach static Public IP on cloud servers")
+	cmd.Flags().BoolVar(&useAWS, "aws", false, "create node/s in AWS cloud")
+	cmd.Flags().BoolVar(&useGCP, "gcp", false, "create node/s in GCP cloud")
+	cmd.Flags().StringVar(&cmdLineRegion, "region", "", "create node/s in given region")
+	cmd.Flags().BoolVar(&authorizeAccess, "authorize-access", false, "authorize CLI to create cloud resources")
+	cmd.Flags().IntVar(&numNodes, "num-nodes", 0, "number of nodes to create")
+	cmd.Flags().StringVar(&cmdLineGCPCredentialsPath, "gcp-credentials", "", "use given GCP credentials")
+	cmd.Flags().StringVar(&cmdLineGCPProjectName, "gcp-project", "", "use given GCP project")
+	cmd.Flags().StringVar(&cmdLineAlternativeKeyPairName, "alternative-key-pair-name", "", "key pair name to use if default one generates conflicts")
+	cmd.Flags().StringVar(&awsProfile, "aws-profile", constants.AWSDefaultCredential, "aws profile to use")
 	return cmd
 }
 
 func wiz(cmd *cobra.Command, args []string) error {
 	clusterName := args[0]
 	subnetName := args[1]
-	_ = clusterName
-	_ = subnetName
-	return nil
+	createDevnet = true
+	useAvalanchegoVersionFromSubnet = subnetName
+	// check there is no clusterName ...
+	// if there is no subnet, create it
+	return createNodes(cmd, []string{clusterName})
 }
