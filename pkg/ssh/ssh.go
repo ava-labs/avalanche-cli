360416b020975826e5fd8bd2ed62027446a8660a
Artur Reznikov <artur.reznikov@avalabs.org>
2023-11-15 16:27:52 -0800

    fix typo

--- pkg/ssh/ssh.go
+++ pkg/ssh/ssh.go
@@ -194,8 +194,8 @@ func RunSSHCheckBootstrapped(host models.Host) ([]byte, error) {
 // RunSSHCheckHealthy checks if node is healthy
 func RunSSHCheckHealthy(host models.Host) ([]byte, error) {
 	// Craft and send the HTTP POST request
-	requestBody := "{\"jsonrpc\":\"2.0\", \"id\":1,\"method\" :\"health.health\"}"
-	return PostOverSSH(host, "ext/health", requestBody)
+	requestBody := "{\"jsonrpc\":\"2.0\", \"id\":1,\"method\":\"health.health\"}"
+	return PostOverSSH(host, "/ext/health", requestBody)
 }
 
 // RunSSHGetNodeID reads nodeID from avalanchego
