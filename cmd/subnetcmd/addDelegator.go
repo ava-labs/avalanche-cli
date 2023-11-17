4af4604a72c6f01b489b7108fa5b591ac986ac12
Felipe Madero <felipe.madero@gmail.com>
2023-11-17 16:03:00 -0300

    stop asking for devnet endpoint on node create

--- cmd/subnetcmd/addDelegator.go
+++ cmd/subnetcmd/addDelegator.go
@@ -77,6 +77,7 @@ func addPermissionlessDelegator(_ *cobra.Command, args []string) error {
 		deployTestnet,
 		deployMainnet,
 		"",
+		true,
 		[]models.NetworkKind{models.Local, models.Fuji, models.Mainnet},
 	)
 	if err != nil {
