9b8e825c1bc9d7737272ff9bbac0c2277d677555
Felipe Madero <felipe.madero@gmail.com>
2023-11-17 20:33:39 -0300

    other one

--- pkg/ssh/shell/setupCLIFromSource.sh
+++ pkg/ssh/shell/setupCLIFromSource.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-#name:TASK [install avalanche-cli from source]
+export PATH=$PATH:~/go/bin
 cd ~
 rm -rf avalanche-cli
 git clone --single-branch -b {{ .CliBranch }} https://github.com/ava-labs/avalanche-cli 
 cd avalanche-cli
-bash -i -c ./scripts/build.sh
+./scripts/build.sh
 cp bin/avalanche ~/bin/avalanche
