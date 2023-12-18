f99e9d437bdf1fbafb708b3ba1ae8af65b01ceb7
arturrez <56270896+arturrez@users.noreply.github.com>
2023-12-18 10:12:39 -0800

    Ssh shell (#1297)
    * add node shell command
    
    * fix nodeID
    
    * move shell functionality to ssh
    
    * use |

--- cmd/nodecmd/ssh.go
+++ cmd/nodecmd/ssh.go
@@ -17,12 +17,14 @@ import (
 
 func newSSHCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "ssh [clusterName] [cmd]",
+		Use:   "ssh [clusterName|nodeID|instanceID|IP] [cmd]",
 		Short: "(ALPHA Warning) Execute ssh command on node(s)",
 		Long: `(ALPHA Warning) This command is currently in experimental mode.
 
-The node ssh command execute a given command using ssh on all nodes in the cluster.
-If no command is given, just prints the ssh cmdLine to be used to connect to each node.
+The node ssh command execute a given command [cmd] using ssh on all nodes in the cluster if ClusterName is given.
+If no command is given, just prints the ssh command to be used to connect to each node in the cluster.
+For provided NodeID or InstanceID or IP, the command [cmd] will be executed on that node.
+If no [cmd] is provided for the node, it will open ssh shell there.
 `,
 		SilenceUsage: true,
 		Args:         cobra.MinimumNArgs(0),
@@ -33,54 +35,111 @@ If no command is given, just prints the ssh cmdLine to be used to connect to eac
 }
 
 func sshNode(_ *cobra.Command, args []string) error {
-	if len(args) == 0 {
-		var err error
-		clustersConfig := models.ClustersConfig{}
-		if app.ClustersConfigExists() {
-			clustersConfig, err = app.LoadClustersConfig()
-			if err != nil {
-				return err
-			}
+	var err error
+	clustersConfig := models.ClustersConfig{}
+	if app.ClustersConfigExists() {
+		clustersConfig, err = app.LoadClustersConfig()
+		if err != nil {
+			return err
 		}
-		if len(clustersConfig.Clusters) == 0 {
-			ux.Logger.PrintToUser("There are no clusters defined.")
-			return nil
-		}
-		for clusterName, clusterConfig := range clustersConfig.Clusters {
-			ux.Logger.PrintToUser("Cluster %q (%s)", clusterName, clusterConfig.Network.Name())
-			if err := sshCluster([]string{clusterName}, "  "); err != nil {
-				return err
-			}
-			ux.Logger.PrintToUser("")
-		}
+	}
+	if len(clustersConfig.Clusters) == 0 {
+		ux.Logger.PrintToUser("There are no clusters defined.")
 		return nil
 	}
-	return sshCluster(args, "")
+	if len(args) == 0 {
+		// provide ssh connection string for all clusters
+		for clusterName, clusterConfig := range clustersConfig.Clusters {
+			return printClusterConnectionString(clusterName, clusterConfig.Network.Name())
+		}
+		return nil
+	} else {
+		clusterNameOrNodeID := args[0]
+		cmd := strings.Join(args[1:], " ")
+		if err := checkCluster(clusterNameOrNodeID); err == nil {
+			// clusterName detected
+			if len(args[1:]) == 0 {
+				return printClusterConnectionString(clusterNameOrNodeID, clustersConfig.Clusters[clusterNameOrNodeID].Network.Name())
+			} else {
+				clusterHosts, err := ansible.GetInventoryFromAnsibleInventoryFile(app.GetAnsibleInventoryDirPath(clusterNameOrNodeID))
+				if err != nil {
+					return err
+				}
+				return sshHosts(clusterHosts, cmd)
+			}
+		} else {
+			// try to detect nodeID
+			for clusterName := range clustersConfig.Clusters {
+				clusterHosts, err := ansible.GetInventoryFromAnsibleInventoryFile(app.GetAnsibleInventoryDirPath(clusterName))
+				if err != nil {
+					return err
+				}
+				selectedHost := utils.Filter(clusterHosts, func(h *models.Host) bool {
+					_, cloudHostID, _ := models.HostAnsibleIDToCloudID(h.NodeID)
+					hostNodeID, _ := getNodeID(app.GetNodeInstanceDirPath(cloudHostID))
+					return h.GetCloudID() == clusterNameOrNodeID || hostNodeID.String() == clusterNameOrNodeID || h.IP == clusterNameOrNodeID
+				})
+				switch {
+				case len(selectedHost) == 0:
+					continue
+				case len(selectedHost) > 2:
+					return fmt.Errorf("more then 1 node found for %s", clusterNameOrNodeID)
+				default:
+					return sshHosts(selectedHost, cmd)
+				}
+			}
+		}
+		return fmt.Errorf("cluster or node %s not found", clusterNameOrNodeID)
+	}
 }
 
-func sshCluster(args []string, indent string) error {
-	clusterName := args[0]
-	if err := checkCluster(clusterName); err != nil {
-		return err
-	}
-	hosts, err := ansible.GetInventoryFromAnsibleInventoryFile(app.GetAnsibleInventoryDirPath(clusterName))
-	if err != nil {
-		return err
-	}
-	for _, host := range hosts {
-		cmdLine := fmt.Sprintf("%s %s", utils.GetSSHConnectionString(host.IP, host.SSHPrivateKeyPath), strings.Join(args[1:], " "))
-		ux.Logger.PrintToUser("%s[%s] %s", indent, host.GetCloudID(), cmdLine)
-		if len(args) > 1 {
+func sshHosts(hosts []*models.Host, cmd string) error {
+	if cmd != "" {
+		// execute cmd
+		for _, host := range hosts {
+			cmdLine := fmt.Sprintf("%s %s", utils.GetSSHConnectionString(host.IP, host.SSHPrivateKeyPath), cmd)
 			splitCmdLine := strings.Split(cmdLine, " ")
 			cmd := exec.Command(splitCmdLine[0], splitCmdLine[1:]...) //nolint: gosec
 			cmd.Env = os.Environ()
 			_, _ = utils.SetupRealtimeCLIOutput(cmd, true, true)
-			err = cmd.Run()
-			if err != nil {
+			if err := cmd.Run(); err != nil {
 				ux.Logger.PrintToUser("Error: %s", err)
 			}
-			ux.Logger.PrintToUser("")
+		}
+	} else {
+		// open shell
+		switch {
+		case len(hosts) > 1:
+			return fmt.Errorf("cannot open ssh shell on multiple nodes: %s", strings.Join(utils.Map(hosts, func(h *models.Host) string { return h.GetCloudID() }), ", "))
+		case len(hosts) == 0:
+			return fmt.Errorf("no nodes found")
+		default:
+			selectedHost := hosts[0]
+			splitCmdLine := strings.Split(utils.GetSSHConnectionString(selectedHost.IP, selectedHost.SSHPrivateKeyPath), " ")
+			cmd := exec.Command(splitCmdLine[0], splitCmdLine[1:]...) //nolint: gosec
+			cmd.Env = os.Environ()
+			cmd.Stdin = os.Stdin
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			if err := cmd.Run(); err != nil {
+				ux.Logger.PrintToUser("Error: %s", err)
+				return err
+			}
+			ux.Logger.PrintToUser("[%s] shell closed to %s", selectedHost.GetCloudID(), selectedHost.IP)
 		}
 	}
+	return nil
+}
+
+func printClusterConnectionString(clusterName string, networkName string) error {
+	ux.Logger.PrintToUser("Cluster %q (%s)", clusterName, networkName)
+	clusterHosts, err := ansible.GetInventoryFromAnsibleInventoryFile(app.GetAnsibleInventoryDirPath(clusterName))
+	if err != nil {
+		return err
+	}
+	for _, host := range clusterHosts {
+		ux.Logger.PrintToUser(utils.GetSSHConnectionString(host.IP, host.SSHPrivateKeyPath))
+	}
+	ux.Logger.PrintToUser("")
 	return nil
 }
