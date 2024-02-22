cc039ffb4f84fbae8de737a03d06f7f727d2c16a
Artur Reznikov <artur.reznikov@avalabs.org>
2024-02-22 15:19:35 -0800

    nit

--- cmd/nodecmd/whitelist-ssh.go
+++ cmd/nodecmd/whitelist-ssh.go
@@ -52,7 +52,7 @@ func whitelistSSH(_ *cobra.Command, args []string) error {
 				nodeResults.AddResult(host.NodeID, nil, err)
 				return
 			}
-			ux.Logger.GreenCheckmarkToUser(utils.ScriptLog(host.NodeID, "Whitelisted SSH public key for node %s"))
+			ux.Logger.GreenCheckmarkToUser(utils.ScriptLog(host.NodeID, "Whitelisted SSH public key"))
 		}(&wgResults, host)
 	}
 	wg.Wait()
