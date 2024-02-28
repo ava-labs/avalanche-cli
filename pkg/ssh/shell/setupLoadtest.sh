cf7da83d28a048b9d85ad6e29cb286f86a276b53
sukantoraymond <rsukanto@umich.edu>
2024-02-28 16:20:25 -0500

    fix script

--- pkg/ssh/shell/setupLoadtest.sh
+++ pkg/ssh/shell/setupLoadtest.sh
@@ -7,7 +7,17 @@ echo "ensuring that gcc is installed ..."
 gcc --version || DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o DPkg::Lock::Timeout=120 install gcc
 # install go
 echo "ensuring that go is installed ..."
-go version || sudo snap install go --classic
+install_go() {
+  GOFILE=go{{ .GoVersion }}.linux-amd64.tar.gz
+  cd ~
+  sudo rm -rf $GOFILE go
+  wget -nv https://go.dev/dl/$GOFILE
+  tar xfz $GOFILE
+  echo >> ~/.bashrc
+  echo export PATH=\$PATH:~/go/bin:~/bin >> ~/.bashrc
+  echo export CGO_ENABLED=1 >> ~/.bashrc
+}
+go version || install_go
 echo "building load test binary ..."
 # run load test build command
 eval {{ .LoadTestPath }}
