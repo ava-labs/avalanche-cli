8af493fe74f3d43d8e2e32b999cd8f9f59f2445b
Felipe Madero <felipe.madero@gmail.com>
2024-03-06 14:11:18 -0300

    nit

--- cmd/nodecmd/status.go
+++ cmd/nodecmd/status.go
@@ -1,4 +1,4 @@
-// Copyri/valght (C) 2022, Ava Labs, Inc. All rights reserved.
+// Copyrigh (C) 2022, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 package nodecmd
 
