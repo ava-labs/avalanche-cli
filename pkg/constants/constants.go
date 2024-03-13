dd23f252eb2a3d303a868f617bdd7cd77e76e43b
Felipe Madero <felipe.madero@gmail.com>
2024-03-13 14:46:12 -0300

    lint

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -50,13 +50,13 @@ const (
 	APIRequestTimeout   = 30 * time.Second
 	FastGRPCDialTimeout = 100 * time.Millisecond
 
-	SSHServerStartTimeout   = 1 * time.Minute
-	SSHScriptTimeout        = 2 * time.Minute
-	SSHDirOpsTimeout        = 10 * time.Second
-	SSHFileOpsTimeout       = 30 * time.Second
-	SSHPOSTTimeout          = 10 * time.Second
-	SSHSleepBetweenChecks   = 1 * time.Second
-	SSHShell                = "/bin/bash"
+	SSHServerStartTimeout = 1 * time.Minute
+	SSHScriptTimeout      = 2 * time.Minute
+	SSHDirOpsTimeout      = 10 * time.Second
+	SSHFileOpsTimeout     = 30 * time.Second
+	SSHPOSTTimeout        = 10 * time.Second
+	SSHSleepBetweenChecks = 1 * time.Second
+	SSHShell              = "/bin/bash"
 
 	SimulatePublicNetwork = "SIMULATE_PUBLIC_NETWORK"
 
