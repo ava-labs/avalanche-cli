8bca74a2c4b1d2646f6a3bd661160a8a99b33d07
Artur Reznikov <artur.reznikov@avalabs.org>
2024-03-14 11:48:42 -0700

    fix grafana server systemctl unit name

--- pkg/ssh/shell/updateGrafanaDashboards.sh
+++ pkg/ssh/shell/updateGrafanaDashboards.sh
@@ -2,4 +2,4 @@
 #name:TASK [sync grafana dashboards]
 sudo cp -Rf /home/ubuntu/dashboards/ /etc/grafana/dashboards/
 #name:TASK [restart prometheus service]
-sudo systemctl restart grafana
+sudo systemctl restart grafana-server
