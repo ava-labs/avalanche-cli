72d43c7d9c58b27349a4bad047c62b3b4a367ca4
Raymond Sukanto <rsukanto@umich.edu>
2024-03-14 00:30:15 -0400

    loadtest commit

--- pkg/ssh/shell/buildLoadTest.sh
+++ pkg/ssh/shell/buildLoadTest.sh
@@ -7,11 +7,11 @@ cd {{ .RepoDirName}}; git checkout {{ .LoadTestGitCommit}}
 {{end}}
 # install gcc
 echo "ensuring that gcc is installed ..."
-#gcc --version || DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o DPkg::Lock::Timeout=120 install gcc
-## install go
-#echo "ensuring that go is installed ..."
-#go version || sudo snap install go --classic
-#echo "building load test binary ..."
-## run load test build command
-#eval {{ .LoadTestPath }}
-#echo "successfully built load test binary!"
+gcc --version || DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o DPkg::Lock::Timeout=120 install gcc
+# install go
+echo "ensuring that go is installed ..."
+go version || sudo snap install go --classic
+echo "building load test binary ..."
+# run load test build command
+eval {{ .LoadTestPath }}
+echo "successfully built load test binary!"
