685374861f19a25a0307e2d583f01c1ef110db3c
Artur Reznikov <artur.reznikov@avalabs.org>
2024-03-14 14:17:43 -0700

    put apt update back

--- pkg/ssh/shell/setupBuildEnv.sh
+++ pkg/ssh/shell/setupBuildEnv.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -e
 #name:TASK [install gcc if not available]
+sudo apt-get -y -o DPkg::Lock::Timeout=120 update
 gcc --version || DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o DPkg::Lock::Timeout=120 install gcc
