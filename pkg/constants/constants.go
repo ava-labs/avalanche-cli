83029fec803596a918ca913c46d7e14bf61cd44d
felipemadero <felipe.madero@gmail.com>
2024-03-21 14:27:02 -0300

    stop compiling cli from source on remote (#1647)

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -160,8 +160,8 @@ const (
 	OutboundPort                                 = 0
 	// Set this one to true while testing changes that alter CLI execution on cloud nodes
 	// Disable it for releases to save cluster creation time
-	EnableSetupCLIFromSource           = true
-	SetupCLIFromSourceBranch           = "relayer-on-devnet"
+	EnableSetupCLIFromSource           = false
+	SetupCLIFromSourceBranch           = "main"
 	BuildEnvGolangVersion              = "1.22.1"
 	IsHealthyJSONFile                  = "isHealthy.json"
 	IsBootstrappedJSONFile             = "isBootstrapped.json"
