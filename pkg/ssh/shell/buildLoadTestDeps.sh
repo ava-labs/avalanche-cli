ab5dbe8f9d94d0a81d7e154346c8fcf7a5265477
Raymond Sukanto <rsukanto@umich.edu>
2024-03-29 18:02:13 -0400

    retry gcc install

--- pkg/ssh/shell/buildLoadTestDeps.sh
+++ pkg/ssh/shell/buildLoadTestDeps.sh
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
 # install gcc
 echo "ensuring that gcc is installed ..."
-while true; do
-    gcc --version >/dev/null 2>&1
-    if [ $? -eq 0 ]; then
-        echo "GCC is installed!"
-        break
-    fi
-
+while ! gcc --version >/dev/null 2>&1; do
     echo "GCC is not installed. Trying to install..."
     DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o DPkg::Lock::Timeout=120 install gcc
-
-    if [ $? -neq 0 ]; then
+    if [ $? -ne 0 ]; then
         echo "Failed to install GCC. Retrying in 10 seconds..."
         sleep 10
     fi
--- pkg/ssh/shell/setupBuildEnv.sh
+++ pkg/ssh/shell/setupBuildEnv.sh
@@ -2,17 +2,10 @@
 set -e
 #name:TASK [install gcc if not available]
 sudo apt-get -y -o DPkg::Lock::Timeout=120 update
-while true; do
-    gcc --version >/dev/null 2>&1
-    if [ $? -eq 0 ]; then
-        echo "GCC is installed!"
-        break
-    fi
-
+while ! gcc --version >/dev/null 2>&1; do
     echo "GCC is not installed. Trying to install..."
     DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o DPkg::Lock::Timeout=120 install gcc
-
-    if [ $? -neq 0 ]; then
+    if [ $? -ne 0 ]; then
         echo "Failed to install GCC. Retrying in 10 seconds..."
         sleep 10
     fi
