46b61e710f389fd11d71eed8d6dd6b7d1dee7982
Raymond Sukanto <rsukanto@umich.edu>
2024-03-29 13:59:58 -0400

    increase gcc timeout

--- pkg/ssh/shell/buildLoadTestDeps.sh
+++ pkg/ssh/shell/buildLoadTestDeps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # install gcc
 echo "ensuring that gcc is installed ..."
-gcc --version || DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o DPkg::Lock::Timeout=120 install gcc
+gcc --version || DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o DPkg::Lock::Timeout=600 install gcc
 # install go
 echo "ensuring that go is installed ..."
 go version || sudo snap install go --classic
--- pkg/ssh/shell/setupBuildEnv.sh
+++ pkg/ssh/shell/setupBuildEnv.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 #name:TASK [install gcc if not available]
-sudo apt-get -y -o DPkg::Lock::Timeout=120 update
-gcc --version || DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o DPkg::Lock::Timeout=120 install gcc
+sudo apt-get -y -o DPkg::Lock::Timeout=600 update
+gcc --version || DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o DPkg::Lock::Timeout=600 install gcc
 #name:TASK [install go]
 install_go() {
   local GOFILE
