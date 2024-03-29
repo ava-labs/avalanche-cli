4d746f7fd46f65b53931bfa4f3c9b0f83e213491
Raymond Sukanto <rsukanto@umich.edu>
2024-03-29 17:13:20 -0400

    improved gcc install confirmation

--- pkg/ssh/shell/buildLoadTest.sh
+++ pkg/ssh/shell/buildLoadTest.sh
@@ -4,8 +4,21 @@
 if [ -d {{ .LoadTestRepoDir }} ]; then
   rm -r {{ .LoadTestRepoDir }}
 fi
-echo "final check to ensure that gcc is installed ..."
-gcc --version || DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o DPkg::Lock::Timeout=120 install gcc
+while true; do
+    gcc --version >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "GCC is installed!"
+        break
+    fi
+
+    echo "GCC is not installed. Trying to install..."
+    DEBIAN_FRONTEND=noninteractive sudo apt-get -y -o DPkg::Lock::Timeout=120 install gcc
+
+    if [ $? -neq 0 ]; then
+        echo "Failed to install GCC. Retrying in 10 seconds..."
+        sleep 10
+    fi
+done
 git clone {{ .LoadTestRepo }}
 echo "getting load test repo ..."
 cd {{ .LoadTestRepoDir }}
