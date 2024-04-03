abfdc79954a3c1987edc44d330163733d4555494
Raymond Sukanto <rsukanto@umich.edu>
2024-04-03 19:43:09 -0400

    change go command

--- pkg/ssh/shell/setupBuildEnv.sh
+++ pkg/ssh/shell/setupBuildEnv.sh
@@ -11,19 +11,20 @@ while ! gcc --version >/dev/null 2>&1; do
     fi
 done
 #name:TASK [install go]
-install_go() {
-  local GOFILE
-  if [[ "$(uname -m)" == "aarch64" ]]; then
-    GOFILE="go{{ .GoVersion }}.linux-arm64.tar.gz"
-  else
-    GOFILE="go{{ .GoVersion }}.linux-amd64.tar.gz"
-  fi
-  cd ~
-  sudo rm -rf $GOFILE go
-  wget -q -nv https://go.dev/dl/$GOFILE
-  tar xfz $GOFILE
-  echo >> ~/.bashrc
-  echo export PATH=\$PATH:~/go/bin:~/bin >> ~/.bashrc
-  echo export CGO_ENABLED=1 >> ~/.bashrc
-}
-go version || install_go
+#install_go() {
+#  local GOFILE
+#  if [[ "$(uname -m)" == "aarch64" ]]; then
+#    GOFILE="go{{ .GoVersion }}.linux-arm64.tar.gz"
+#  else
+#    GOFILE="go{{ .GoVersion }}.linux-amd64.tar.gz"
+#  fi
+#  cd ~
+#  sudo rm -rf $GOFILE go
+#  wget -q -nv https://go.dev/dl/$GOFILE
+#  tar xfz $GOFILE
+#  echo >> ~/.bashrc
+#  echo export PATH=\$PATH:~/go/bin:~/bin >> ~/.bashrc
+#  echo export CGO_ENABLED=1 >> ~/.bashrc
+#}
+#go version || install_go
+go version || sudo snap install go --classic
