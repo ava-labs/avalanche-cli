04e703cf45ca14c26ca3f05de951f5e9b02d90e2
Raymond Sukanto <rsukanto@umich.edu>
2024-04-03 18:31:28 -0400

    update logs

--- cmd/nodecmd/wiz.go
+++ cmd/nodecmd/wiz.go
@@ -470,7 +470,7 @@ func updateProposerVMs(
 			return err
 		}
 		if deployedSubnetSc.TeleporterReady && deployedSubnetIsEVMGenesis {
-			ux.Logger.PrintToUser("updating proposerVM on %s", deployedSubnetName)
+			ux.Logger.PrintToUser("Updating proposerVM on %s", deployedSubnetName)
 			blockchainID := deployedSubnetSc.Networks[network.Name()].BlockchainID
 			if blockchainID == ids.Empty {
 				return ErrNoBlockchainID
@@ -480,7 +480,7 @@ func updateProposerVMs(
 			}
 		}
 	}
-	ux.Logger.PrintToUser("updating proposerVM on c-chain")
+	ux.Logger.PrintToUser("Updating proposerVM on c-chain")
 	return teleporter.SetProposerVM(app, network, "C", "")
 }
 
