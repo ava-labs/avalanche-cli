5ae709c2a162ff780af3806f14c76472271ccfb9
felipemadero <felipe.madero@gmail.com>
2024-04-04 16:18:15 -0300

    fix E2E attempt 1 (#1722)

--- pkg/ssh/shell/setupBuildEnv.sh
+++ pkg/ssh/shell/setupBuildEnv.sh
@@ -5,4 +5,7 @@ export DEBIAN_FRONTEND=noninteractive
 until sudo apt-get -y update -o DPkg::Lock::Timeout=120; do sleep 10 && echo "Try again"; done
 until sudo apt-get -y install -o DPkg::Lock::Timeout=120 gcc; do sleep 10 && echo "Try again"; done
 #name:TASK [install go]
+{{if .IsE2E }}
+sudo apt-get -y install snap
+{{end}}
 go version || sudo snap install go --classic
