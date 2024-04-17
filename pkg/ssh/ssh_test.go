2a90152598b92338fd11c3bbca50911d76668793
arturrez <56270896+arturrez@users.noreply.github.com>
2024-04-17 11:20:47 -0700

    rm sed as external dependency for custom grafana dashboard (#1775)
    * rm sed as external dependency for custom grafana dashboard
    
    * lint
    
    * fix unused param
    
    * fix

---
+++ pkg/ssh/ssh_test.go
@@ -0,0 +1,93 @@
+// Copyright (C) 2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+package ssh
+
+import (
+	"os"
+	"testing"
+
+	"github.com/ava-labs/avalanche-cli/pkg/constants"
+)
+
+func TestReplaceCustomVarDashboardValues(t *testing.T) {
+	tempFileName := "test_dashboard.json"
+	tempContent := []byte(`{
+		"templating": {
+			"list": [
+				{
+					"current": {
+						"selected": true,
+						"text": "CHAIN_ID_VAL",
+						"value": "CHAIN_ID_VAL"
+					},
+					"hide": 0,
+					"includeAll": false,
+					"multi": false,
+					"name": "CHAIN_ID",
+					"options": [
+						{
+							"selected": true,
+							"text": "CHAIN_ID_VAL",
+							"value": "CHAIN_ID_VAL"
+						}
+					],
+					"query": "CHAIN_ID_VAL",
+					"queryValue": "",
+					"skipUrlSync": false,
+					"type": "custom"
+				}
+			]
+		}
+	}`)
+	err := os.WriteFile(tempFileName, tempContent, constants.WriteReadUserOnlyPerms)
+	if err != nil {
+		t.Fatalf("Error creating test file: %v", err)
+	}
+	defer func() {
+		err := os.WriteFile(tempFileName, []byte{}, constants.WriteReadUserOnlyPerms)
+		if err != nil {
+			t.Fatalf("Error cleaning up test file: %v", err)
+		}
+	}()
+
+	err = replaceCustomVarDashboardValues(tempFileName, "newChainID")
+	if err != nil {
+		t.Fatalf("Error replacing custom variables: %v", err)
+	}
+	modifiedContent, err := os.ReadFile(tempFileName)
+	if err != nil {
+		t.Fatalf("Error reading modified content: %v", err)
+	}
+
+	expectedContent := `{
+		"templating": {
+			"list": [
+				{
+					"current": {
+						"selected": true,
+						"text": "newChainID",
+						"value": "newChainID"
+					},
+					"hide": 0,
+					"includeAll": false,
+					"multi": false,
+					"name": "CHAIN_ID",
+					"options": [
+						{
+							"selected": true,
+							"text": "newChainID",
+							"value": "newChainID"
+						}
+					],
+					"query": "newChainID",
+					"queryValue": "",
+					"skipUrlSync": false,
+					"type": "custom"
+				}
+			]
+		}
+	}`
+	if string(modifiedContent) != expectedContent {
+		t.Errorf("Expected content after replacement:\n%s\nGot:\n%s", expectedContent, string(modifiedContent))
+	}
+}
