6f80bc0973785848b87e7025e96ccb5b8a7d1ecf
Artur Reznikov <artur.reznikov@avalabs.org>
2024-04-18 16:36:53 -0700

    compose wip

--- pkg/docker/compose.go
+++ pkg/docker/compose.go
@@ -3,7 +3,102 @@
 
 package docker
 
-func AvalanchegoComposeFile(withMonitoring bool) ([]bytes, error) {
+import (
+	"bytes"
+	"embed"
+	"fmt"
+	"html/template"
+	"os"
+	"path/filepath"
+	"time"
 
-	return true
+	"github.com/ava-labs/avalanche-cli/pkg/constants"
+	"github.com/ava-labs/avalanche-cli/pkg/models"
+	"github.com/ava-labs/avalanche-cli/pkg/utils"
+	"github.com/ava-labs/avalanche-cli/pkg/ux"
+)
+
+type dockerComposeInputs struct {
+	IsMonitoringEnabled bool
+	AvalancheGoVersion  string
+}
+
+//go:embed templates/*.docker-compose.yml
+var composeTemplate embed.FS
+
+func renderComposeFile(composePath string, composeDesc string, templateVars dockerComposeInputs) ([]byte, error) {
+	compose, err := composeTemplate.ReadFile(composePath)
+	if err != nil {
+		return nil, err
+	}
+	var composeBytes bytes.Buffer
+	t, err := template.New(composeDesc).Parse(string(compose))
+	if err != nil {
+		return nil, err
+	}
+	if err := t.Execute(&composeBytes, templateVars); err != nil {
+		return nil, err
+	}
+	return composeBytes.Bytes(), nil
+}
+
+func pushComposeFile(host *models.Host, localFile string, remoteFile string) error {
+	if !utils.FileExists(localFile) {
+		return fmt.Errorf("file %s does not exist", localFile)
+	}
+	if err := host.MkdirAll(filepath.Dir(remoteFile), constants.SSHFileOpsTimeout); err != nil {
+		return err
+	}
+	if err := host.Upload(localFile, remoteFile, constants.SSHFileOpsTimeout); err != nil {
+		return err
+	}
+	return nil
+}
+
+func StartDockerCompose(host *models.Host, composeFile string, timeout time.Duration) error {
+	if output, err := host.Command(fmt.Sprintf("docker-compose -f %s up -d", composeFile), nil, timeout); err != nil {
+		return fmt.Errorf("%w: %s", err, string(output))
+	}
+	return nil
+}
+
+func ComposeOverSSH(
+	composeDesc string,
+	host *models.Host,
+	timeout time.Duration,
+	composePath string,
+	composeVars dockerComposeInputs,
+) error {
+	remoteComposeFile := filepath.Join(constants.CloudNodeCLIConfigBasePath, "docker-compose.yml")
+	startTime := time.Now()
+	tmpFile, err := os.CreateTemp("", "avalanchecli-docker-compose-*.yml")
+	if err != nil {
+		return err
+	}
+	defer os.Remove(tmpFile.Name())
+	composeData, err := renderComposeFile(composePath, composeDesc, composeVars)
+	if err != nil {
+		return err
+	}
+
+	if _, err := tmpFile.Write(composeData); err != nil {
+		return err
+	}
+	if err := pushComposeFile(host, tmpFile.Name(), remoteComposeFile); err != nil {
+		return err
+	}
+	if err := StartDockerCompose(host, remoteComposeFile, timeout); err != nil {
+		return err
+	}
+	executionTime := time.Since(startTime)
+	ux.Logger.Info("ComposeOverSSH[%s]%s took %s with err: %v", host.NodeID, composeDesc, executionTime, err)
+	return nil
+}
+
+func ComposeSSHSetupNode(host *models.Host, avalancheGoVersion string) error {
+	return ComposeOverSSH("Setup Node",
+		host,
+		constants.SSHScriptTimeout,
+		"templates/avalanchego.docker-compose.yml",
+		dockerComposeInputs{AvalancheGoVersion: avalancheGoVersion})
 }
--- pkg/ssh/shell/setupNode.sh
+++ pkg/ssh/shell/setupNode.sh
@@ -4,28 +4,13 @@ export PATH=$PATH:~/go/bin
 #name:TASK [disable systemctl]
 sudo cp -vf /usr/bin/true /usr/local/sbin/systemctl
 {{end}}
-#name:TASK [update apt data and install dependencies]
-export DEBIAN_FRONTEND=noninteractive
-until sudo apt-get -y install -o DPkg::Lock::Timeout=120 wget curl git; do sleep 10 && echo "Try again"; done
 #name:TASK [create .avalanche-cli .avalanchego dirs]
 mkdir -p .avalanche-cli .avalanchego/staking
-#name:TASK [get avalanche go script]
-wget -q -nd -m https://raw.githubusercontent.com/ava-labs/avalanche-docs/master/scripts/avalanchego-installer.sh
-#name:TASK [modify permissions]
-chmod 755 avalanchego-installer.sh
 #name:TASK [call avalanche go install script]
-./avalanchego-installer.sh --ip static --rpc private --state-sync on --fuji --version {{ .AvalancheGoVersion }}
+#./avalanchego-installer.sh --ip static --rpc private --state-sync on --fuji --version {{ .AvalancheGoVersion }}
 #name:TASK [get avalanche cli install script]
-wget -q -nd -m https://raw.githubusercontent.com/ava-labs/avalanche-cli/main/scripts/install.sh
+busybox wget -q -nd https://raw.githubusercontent.com/ava-labs/avalanche-cli/main/scripts/install.sh
 #name:TASK [modify permissions]
 chmod 755 install.sh
 #name:TASK [run install script]
 ./install.sh -n {{ .CLIVersion }}
-{{if .IsDevNet}}
-#name:TASK [stop avalanchego in case of devnet]
-{{if .IsE2E }}
-sudo pkill avalanchego || echo "avalanchego not running"
-{{ else }}
-sudo systemctl stop avalanchego
-{{end}}
-{{end}}
