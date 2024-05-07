1cecf08f46965479063bba33c9409392e2677b05
Artur Reznikov <artur.reznikov@avalabs.org>
2024-05-07 15:49:36 -0700

    replace mkdir with install -d

--- pkg/ssh/shell/runLoadTest.sh
+++ pkg/ssh/shell/runLoadTest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # run load test command
-mkdir -p `dirname {{ .LoadTestResultFile }}`
+install -g ubuntu  -o ubuntu -d `dirname {{ .LoadTestResultFile }}`
 if [ -e {{ .LoadTestResultFile }} ]; then
   rm {{ .LoadTestResultFile }}
 fi
