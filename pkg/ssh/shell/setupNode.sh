e1c6b11d21befb9fd2119f57cf65957e7145d144
Artur Reznikov <artur.reznikov@avalabs.org>
2024-05-15 15:49:38 -0700

    w

--- pkg/ssh/shell/setupNode.sh
+++ pkg/ssh/shell/setupNode.sh
@@ -12,10 +12,6 @@ echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker
 sudo apt-get -y update && sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
 sudo usermod -aG docker ubuntu
 {{ end }}
-{{ if .IsE2E }}
-echo "E2E skip install"
-exit 0
-{{ end }}
 rm -vf install.sh &&  busybox wget -q -nd https://raw.githubusercontent.com/ava-labs/avalanche-cli/main/scripts/install.sh
 #name:TASK [modify permissions]
 chmod 755 install.sh
