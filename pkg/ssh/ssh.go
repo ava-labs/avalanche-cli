50ba9a2ffa6f1ac50d70cd6b88103d4a3481f8b5
Artur Reznikov <artur.reznikov@avalabs.org>
2024-05-23 15:40:52 -0400

    solve e2e by using lo aliases

--- pkg/ssh/ssh.go
+++ pkg/ssh/ssh.go
@@ -143,6 +143,10 @@ func RunSSHSetupDockerService(host *models.Host) error {
 // RunSSHRestartNode runs script to restart avalanchego
 func RunSSHRestartNode(host *models.Host) error {
 	remoteComposeFile := utils.GetRemoteComposeFile()
+	serviceName := "avalanchego"
+	if utils.IsE2E() {
+		serviceName = serviceName + utils.E2ESuffix(host.IP)
+	}
 	return docker.RestartDockerComposeService(host, remoteComposeFile, "avalanchego", constants.SSHLongRunningScriptTimeout)
 }
 
