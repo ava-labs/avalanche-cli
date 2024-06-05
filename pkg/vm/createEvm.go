c45d2799958c9c1fa2e3741d6a9c3f02d1fd6940
Raymond Sukanto <rsukanto@umich.edu>
2024-06-05 19:55:03 -0400

    remove commented out

--- pkg/vm/createEvm.go
+++ pkg/vm/createEvm.go
@@ -3,8 +3,6 @@
 package vm
 
 import (
-	"bytes"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/ava-labs/avalanche-tooling-sdk-go/subnet"
@@ -18,11 +16,9 @@ import (
 	"github.com/ava-labs/avalanche-cli/pkg/constants"
 	"github.com/ava-labs/avalanche-cli/pkg/models"
 	"github.com/ava-labs/avalanche-cli/pkg/statemachine"
-	"github.com/ava-labs/avalanche-cli/pkg/teleporter"
 	"github.com/ava-labs/avalanche-cli/pkg/ux"
 	"github.com/ava-labs/subnet-evm/core"
 	"github.com/ava-labs/subnet-evm/params"
-	"github.com/ava-labs/subnet-evm/precompile/contracts/txallowlist"
 	"github.com/ava-labs/subnet-evm/utils"
 	"github.com/ethereum/go-ethereum/common"
 )
@@ -113,138 +109,6 @@ func CreateEvmSubnetConfig(
 	return genesisBytes, sc, nil
 }
 
-func createEvmGenesis(
-	app *application.Avalanche,
-	subnetName string,
-	subnetEVMVersion string,
-	rpcVersion int,
-	subnetEVMChainID uint64,
-	subnetEVMTokenSymbol string,
-	useSubnetEVMDefaults bool,
-	useWarp bool,
-	teleporterInfo *teleporter.Info,
-) ([]byte, *models.Sidecar, error) {
-	ux.Logger.PrintToUser("creating genesis for subnet %s", subnetName)
-
-	genesis := core.Genesis{}
-	genesis.Timestamp = *utils.TimeToNewUint64(time.Now())
-
-	conf := params.SubnetEVMDefaultChainConfig
-	conf.NetworkUpgrades = params.NetworkUpgrades{}
-
-	const (
-		descriptorsState = "descriptors"
-		feeState         = "fee"
-		airdropState     = "airdrop"
-		precompilesState = "precompiles"
-	)
-
-	var (
-		chainID     *big.Int
-		tokenSymbol string
-		allocation  core.GenesisAlloc
-		direction   statemachine.StateDirection
-		err         error
-	)
-
-	subnetEvmState, err := statemachine.NewStateMachine(
-		[]string{descriptorsState, feeState, airdropState, precompilesState},
-	)
-	if err != nil {
-		return nil, nil, err
-	}
-	for subnetEvmState.Running() {
-		switch subnetEvmState.CurrentState() {
-		case descriptorsState:
-			chainID, tokenSymbol, direction, err = getDescriptors(
-				app,
-				subnetEVMChainID,
-				subnetEVMTokenSymbol,
-			)
-		case feeState:
-			*conf, direction, err = GetFeeConfig(*conf, app, useSubnetEVMDefaults)
-		case airdropState:
-			allocation, direction, err = getAllocation(
-				app,
-				subnetName,
-				defaultEvmAirdropAmount,
-				oneAvax,
-				fmt.Sprintf("Amount to airdrop (in %s units)", tokenSymbol),
-				useSubnetEVMDefaults,
-			)
-			if teleporterInfo != nil {
-				allocation = addTeleporterAddressToAllocations(
-					allocation,
-					teleporterInfo.FundedAddress,
-					teleporterInfo.FundedBalance,
-				)
-			}
-		case precompilesState:
-			*conf, direction, err = getPrecompiles(*conf, app, &genesis.Timestamp, useSubnetEVMDefaults, useWarp)
-			if teleporterInfo != nil {
-				*conf = addTeleporterAddressesToAllowLists(
-					*conf,
-					teleporterInfo.FundedAddress,
-					teleporterInfo.MessengerDeployerAddress,
-					teleporterInfo.RelayerAddress,
-				)
-			}
-		default:
-			err = errors.New("invalid creation stage")
-		}
-		if err != nil {
-			return nil, nil, err
-		}
-		subnetEvmState.NextState(direction)
-	}
-
-	if conf != nil && conf.GenesisPrecompiles[txallowlist.ConfigKey] != nil {
-		allowListCfg, ok := conf.GenesisPrecompiles[txallowlist.ConfigKey].(*txallowlist.Config)
-		if !ok {
-			return nil, nil, fmt.Errorf(
-				"expected config of type txallowlist.AllowListConfig, but got %T",
-				allowListCfg,
-			)
-		}
-
-		if err := ensureAdminsHaveBalance(
-			allowListCfg.AdminAddresses,
-			allocation); err != nil {
-			return nil, nil, err
-		}
-	}
-
-	conf.ChainID = chainID
-
-	genesis.Alloc = allocation
-	genesis.Config = conf
-	genesis.Difficulty = Difficulty
-	genesis.GasLimit = conf.FeeConfig.GasLimit.Uint64()
-
-	jsonBytes, err := genesis.MarshalJSON()
-	if err != nil {
-		return nil, nil, err
-	}
-
-	var prettyJSON bytes.Buffer
-	err = json.Indent(&prettyJSON, jsonBytes, "", "    ")
-	if err != nil {
-		return nil, nil, err
-	}
-
-	sc := &models.Sidecar{
-		Name:        subnetName,
-		VM:          models.SubnetEvm,
-		VMVersion:   subnetEVMVersion,
-		RPCVersion:  rpcVersion,
-		Subnet:      subnetName,
-		TokenSymbol: tokenSymbol,
-		TokenName:   tokenSymbol + " Token",
-	}
-
-	return prettyJSON.Bytes(), sc, nil
-}
-
 func createEvmGenesisBytes(
 	app *application.Avalanche,
 	subnetName string,
@@ -304,23 +168,8 @@ func createEvmGenesisBytes(
 				fmt.Sprintf("Amount to airdrop (in %s units)", tokenSymbol),
 				useSubnetEVMDefaults,
 			)
-			//if teleporterInfo != nil {
-			//	allocation = addTeleporterAddressToAllocations(
-			//		allocation,
-			//		teleporterInfo.FundedAddress,
-			//		teleporterInfo.FundedBalance,
-			//	)
-			//}
 		case precompilesState:
 			*conf, direction, err = getPrecompiles(*conf, app, &genesis.Timestamp, useSubnetEVMDefaults, useWarp)
-			//if teleporterInfo != nil {
-			//	*conf = addTeleporterAddressesToAllowLists(
-			//		*conf,
-			//		teleporterInfo.FundedAddress,
-			//		teleporterInfo.MessengerDeployerAddress,
-			//		teleporterInfo.RelayerAddress,
-			//	)
-			//}
 		default:
 			err = errors.New("invalid creation stage")
 		}
@@ -329,40 +178,6 @@ func createEvmGenesisBytes(
 		}
 		subnetEvmState.NextState(direction)
 	}
-
-	//if conf != nil && conf.GenesisPrecompiles[txallowlist.ConfigKey] != nil {
-	//	allowListCfg, ok := conf.GenesisPrecompiles[txallowlist.ConfigKey].(*txallowlist.Config)
-	//	if !ok {
-	//		return nil, nil, fmt.Errorf(
-	//			"expected config of type txallowlist.AllowListConfig, but got %T",
-	//			allowListCfg,
-	//		)
-	//	}
-	//
-	//	if err := ensureAdminsHaveBalance(
-	//		allowListCfg.AdminAddresses,
-	//		allocation); err != nil {
-	//		return nil, nil, err
-	//	}
-	//}
-
-	//conf.ChainID = chainID
-
-	//genesis.Alloc = allocation
-	//genesis.Config = conf
-	//genesis.Difficulty = Difficulty
-	//genesis.GasLimit = conf.FeeConfig.GasLimit.Uint64()
-	//
-	//jsonBytes, err := genesis.MarshalJSON()
-	//if err != nil {
-	//	return nil, nil, err
-	//}
-	//
-	//var prettyJSON bytes.Buffer
-	//err = json.Indent(&prettyJSON, jsonBytes, "", "    ")
-	//if err != nil {
-	//	return nil, nil, err
-	//}
 
 	sc := &models.Sidecar{
 		Name:        subnetName,
