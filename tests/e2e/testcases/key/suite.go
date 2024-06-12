49a7561d71dcd49b30f6c5b726ff25c94ed5855c
Felipe Madero <felipe.madero@gmail.com>
2024-06-11 23:31:51 -0300

    fix e2e

--- tests/e2e/testcases/key/suite.go
+++ tests/e2e/testcases/key/suite.go
@@ -132,7 +132,7 @@ var _ = ginkgo.Describe("[Key]", func() {
 		// if there are independent regexes instead of one large one,
 		// difficult to understand (go regexes don't support Perl regex
 		// Go RE2 library doesn't support lookahead and lookbehind
-		regex1 := `.*NAME.*CHAIN.*ADDRESS.*NETWORK`
+		regex1 := `.*NAME.*SUBNET.*ADDRESS.*NETWORK`
 		regex2 := `.*e2eKey.*C-Chain.*0x[a-fA-F0-9]{40}`
 		regex3 := `.*P-Chain.*[(P-custom)(P-fuji)][a-zA-Z0-9]{39}`
 		regex4 := `.*P-avax[a-zA-Z0-9]{39}`
