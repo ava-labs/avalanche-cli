024f01744b7c93b2c76ce2104de4da2439199973
Felipe Madero <felipe.madero@gmail.com>
2024-06-22 21:25:09 -0300

    onyl refactoring is needed

--- cmd/teleportercmd/relayercmd/logs.go
+++ cmd/teleportercmd/relayercmd/logs.go
@@ -3,19 +3,31 @@
 package relayercmd
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
 	"os"
+	"sort"
+	"strings"
+	"time"
 
+	"github.com/ava-labs/avalanche-cli/cmd/teleportercmd/bridgecmd"
 	"github.com/ava-labs/avalanche-cli/pkg/cobrautils"
 	"github.com/ava-labs/avalanche-cli/pkg/models"
 	"github.com/ava-labs/avalanche-cli/pkg/networkoptions"
+	"github.com/ava-labs/avalanche-cli/pkg/utils"
+	"github.com/ava-labs/avalanchego/utils/logging"
 
+	"github.com/jedib0t/go-pretty/v6/table"
+	"github.com/mitchellh/go-wordwrap"
 	"github.com/spf13/cobra"
+	"golang.org/x/exp/maps"
 )
 
 var (
-	logsNetworkOptions = []networkoptions.NetworkOption{networkoptions.Local, networkoptions.Cluster}
+	logsNetworkOptions = []networkoptions.NetworkOption{networkoptions.Local}
+	raw                bool
+	last               uint
+	first              uint
 )
 
 // avalanche teleporter relayer logs
@@ -28,6 +40,9 @@ func newLogsCmd() *cobra.Command {
 		Args:  cobrautils.ExactArgs(0),
 	}
 	networkoptions.AddNetworkFlagsToCmd(cmd, &globalNetworkFlags, true, logsNetworkOptions)
+	cmd.Flags().BoolVar(&raw, "raw", false, "raw logs output")
+	cmd.Flags().UintVar(&last, "last", 0, "output last N log lines")
+	cmd.Flags().UintVar(&first, "first", 0, "output first N log lines")
 	return cmd
 }
 
@@ -44,22 +59,159 @@ func logs(_ *cobra.Command, _ []string) error {
 	if err != nil {
 		return err
 	}
-	switch {
-	case network.Kind == models.Local:
-		logsPath := app.GetAWMRelayerLogPath()
-		file, err := os.Open(logsPath)
+	subnetNames, err := app.GetSubnetNamesOnNetwork(network)
+	if err != nil {
+		return err
+	}
+	blockchainIDToSubnetName := map[string]string{}
+	for _, subnetName := range subnetNames {
+		_, _, blockchainID, _, _, _, err := bridgecmd.GetSubnetParams(network, subnetName, false)
 		if err != nil {
 			return err
 		}
-		defer file.Close()
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			fmt.Println(scanner.Text())
-		}
-		if err := scanner.Err(); err != nil {
+		blockchainIDToSubnetName[blockchainID.String()] = subnetName
+	}
+	_, _, blockchainID, _, _, _, err := bridgecmd.GetSubnetParams(network, "", true)
+	if err != nil {
+		return err
+	}
+	blockchainIDToSubnetName[blockchainID.String()] = "c-chain"
+	var logLines []string
+	switch {
+	case network.Kind == models.Local:
+		logsPath := app.GetAWMRelayerLogPath()
+		bs, err := os.ReadFile(logsPath)
+		if err != nil {
 			return err
 		}
-	case network.ClusterName != "":
+		logs := string(bs)
+		logLines = strings.Split(logs, "\n")
+	default:
+		return fmt.Errorf("unsupported network")
 	}
+	if first != 0 {
+		if len(logLines) > int(first) {
+			logLines = logLines[:first]
+		}
+	}
+	if last != 0 {
+		if len(logLines) > int(last) {
+			logLines = logLines[len(logLines)-1-int(last):]
+		}
+	}
+	if raw {
+		for _, logLine := range logLines {
+			logLine = strings.TrimSpace(logLine)
+			if len(logLine) != 0 {
+				fmt.Println(logLine)
+			}
+		}
+		return nil
+	}
+	t := table.NewWriter()
+	t.AppendHeader(table.Row{"", "Time", "Chain", "Log"})
+	for _, logLine := range logLines {
+		logLine = strings.TrimSpace(logLine)
+		if len(logLine) != 0 {
+			logMap := map[string]interface{}{}
+			err := json.Unmarshal([]byte(logLine), &logMap)
+			if err != nil {
+				return err
+			}
+			levelEmoji := ""
+			levelStr, b := logMap["level"].(string)
+			if b {
+				level, err := logging.ToLevel(levelStr)
+				if err != nil {
+					return err
+				}
+				switch level {
+				case logging.Info:
+					levelEmoji = "ℹ️"
+				case logging.Debug:
+					levelEmoji = "🪲"
+				case logging.Warn:
+					levelEmoji = "⚠️"
+				case logging.Error:
+					levelEmoji = "⛔"
+				case logging.Fatal:
+					levelEmoji = "💀"
+				}
+			}
+			timeStampStr, b := logMap["timestamp"].(string)
+			timeStr := ""
+			if b {
+				t, err := time.Parse("2006-01-02T15:04:05.000-0700", timeStampStr)
+				if err != nil {
+					return err
+				}
+				timeStr = t.Format("15:04:05")
+			}
+			msg, b := logMap["msg"].(string)
+			if !b {
+				continue
+			}
+			logMsg := wordwrap.WrapString(msg, 80)
+			logMsgLines := strings.Split(logMsg, "\n")
+			logMsgLines = utils.Map(logMsgLines, func(s string) string { return logging.Green.Wrap(s) })
+			logMsg = strings.Join(logMsgLines, "\n")
+			keys := maps.Keys(logMap)
+			sort.Strings(keys)
+			for _, k := range keys {
+				if !utils.Belongs([]string{"logger", "caller", "level", "timestamp", "msg"}, k) {
+					logMsg = addAditionalInfo(
+						logMsg,
+						logMap,
+						k,
+						k,
+						blockchainIDToSubnetName,
+					)
+				}
+			}
+			subnet := getLogSubnet(logMap, blockchainIDToSubnetName)
+			t.AppendRow(table.Row{levelEmoji, timeStr, subnet, logMsg})
+		}
+	}
+	fmt.Println(t.Render())
+
 	return nil
+}
+
+func addAditionalInfo(
+	logMsg string,
+	logMap map[string]interface{},
+	key string,
+	outputName string,
+	blockchainIDToSubnetName map[string]string,
+) string {
+	value, b := logMap[key].(string)
+	if b {
+		subnetName := blockchainIDToSubnetName[value]
+		if subnetName != "" {
+			value = subnetName
+		}
+		logMsg = fmt.Sprintf("%s\n  %s=%s", logMsg, outputName, value)
+	}
+	return logMsg
+}
+
+func getLogSubnet(
+	logMap map[string]interface{},
+	blockchainIDToSubnetName map[string]string,
+) string {
+	for _, key := range []string{
+		"blockchainID",
+		"originBlockchainID",
+		"sourceBlockchainID",
+		"destinationBlockchainID",
+	} {
+		value, b := logMap[key].(string)
+		if b {
+			subnetName := blockchainIDToSubnetName[value]
+			if subnetName != "" {
+				return subnetName
+			}
+		}
+	}
+	return ""
 }
