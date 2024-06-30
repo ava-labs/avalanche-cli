8b6dac2b45daa9dc2140db8d06e060fb729fccd2
Felipe Madero <felipe.madero@gmail.com>
2024-06-30 10:38:21 -0300

    base for contract command

---
+++ pkg/contract/contracts/src/Token.sol
@@ -0,0 +1,14 @@
+// (c) 2024, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+// SPDX-License-Identifier: Ecosystem
+
+pragma solidity ^ 0.8.18;
+
+import "@openzeppelin/contracts@4.8.1/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor(string memory symbol, uint256 balance) ERC20(string.concat(symbol, " Token"), symbol) {
+        _mint(msg.sender, balance * 10 ** decimals());
+    }
+}
---
+++ scripts/build_contracts.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+# go to contracts dir
+base_dir=$(dirname $0)/..
+cd $base_dir/pkg/contract/contracts/
+# prepare build env
+touch foundry.toml
+echo @openzeppelin/contracts@4.8.1/=lib/openzeppelin-contracts/contracts/ > remappings.txt
+[ ! -d lib/openzeppelin-contracts ] && git clone https://github.com/OpenZeppelin/openzeppelin-contracts lib/openzeppelin-contracts -b v4.8.1
+# build 
+forge build --extra-output-files bin
+mkdir -p bin
+cp out/Token.sol/Token.bin bin
+# cleanup
+rm -rf foundry.toml remappings.txt lib cache out
