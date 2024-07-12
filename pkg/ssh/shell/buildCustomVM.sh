84494a0f81a30b5d75f44024d214f2289d55702f
Artur Reznikov <artur.reznikov@avalabs.org>
2024-07-12 13:07:27 -0700

    typo

--- pkg/ssh/shell/buildCustomVM.sh
+++ pkg/ssh/shell/buildCustomVM.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 if [ -d {{ .CustomVMRepoDir }} ]; then
-  rm -rf {{ .LoadTestRepoDir }}
+  rm -rf {{ .CustomVMRepoDir }}
 fi
 
 cd {{ .CustomVMRepoDir }}
-git remote init -q
+git init -q
 git remote add origin {{ .CustomVMRepoURL }}
 git fetch --depth 1 origin {{ .CustomVMBranch }} -q
 git checkout {{ .CustomVMBranch }}
