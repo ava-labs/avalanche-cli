75755817a82531d1758fea983dab4eda8b8e0c8b
Felipe Madero <felipe.madero@gmail.com>
2024-07-18 00:11:20 -0300

    lint

--- pkg/precompiles/precompiles.go
+++ pkg/precompiles/precompiles.go
@@ -8,6 +8,4 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-var (
-	NativeMinterPrecompile = common.HexToAddress("0x0200000000000000000000000000000000000001")
-)
+var NativeMinterPrecompile = common.HexToAddress("0x0200000000000000000000000000000000000001")
