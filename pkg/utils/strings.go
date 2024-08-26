d8553b8da158faf67e2141b690ec19cd5a195b59
Michael Kaplan <michael@avalabs.org>
2024-08-26 16:17:22 -0400

    Add format unit test

--- pkg/utils/strings.go
+++ pkg/utils/strings.go
@@ -64,6 +64,7 @@ func CleanupStrings(s []string) []string {
 }
 
 // Formats an amount of base units as a string representing the amount in the given denomination.
+// (i.e. An amount of 54321 with a decimals value of 3 results in the stirng "54.321")
 func FormatAmount(amount *big.Int, decimals uint8) string {
 	amountFloat := new(big.Float).SetInt(amount)
 	divisor := new(big.Float).SetFloat64(math.Pow10(int(decimals)))
--- pkg/utils/strings_test.go
+++ pkg/utils/strings_test.go
@@ -3,6 +3,7 @@
 package utils
 
 import (
+	"math/big"
 	"reflect"
 	"testing"
 )
@@ -24,5 +25,48 @@ func TestSpitStringWithQuotes(t *testing.T) {
 	result1 := SplitStringWithQuotes(input1, ' ')
 	if !reflect.DeepEqual(result1, expected1) {
 		t.Errorf("Expected %v, but got %v", expected1, result1)
+	}
+}
+
+func TestFormatAmount(t *testing.T) {
+	testCases := []struct {
+		name     string
+		amount   uint64
+		decimals uint8
+		expected string
+	}{
+		{
+			name:     "greater than 1",
+			amount:   54321,
+			decimals: 3,
+			expected: "54.321",
+		},
+		{
+			name:     "less than 1",
+			amount:   1,
+			decimals: 10,
+			expected: "0.0000000001",
+		},
+		{
+			name:     "18 decimals",
+			amount:   9988776655443322110,
+			decimals: 18,
+			expected: "9.988776655443322110",
+		},
+		{
+			name:     "9 decimals, all zeros",
+			amount:   5000000000,
+			decimals: 9,
+			expected: "5.000000000",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := FormatAmount(new(big.Int).SetUint64(tc.amount), tc.decimals)
+			if result != tc.expected {
+				t.Errorf("Expected %s, but got %s", tc.expected, result)
+			}
+		})
 	}
 }
