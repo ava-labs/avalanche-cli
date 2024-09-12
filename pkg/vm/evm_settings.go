900c57812fd9475b4f4c5acae19ef8756fd3c73a
sukantoraymond <rsukanto@umich.edu>
2024-09-12 16:33:50 -0400

    SDK refactor from tooling sdk repo (#2137)
    * sdk refactor
    
    * udpate avalanche sdk
    
    * update pkg
    
    * update ci
    
    * Subnet sdk refactor (#2138)
    
    * refactor subnet sdk create
    
    * update pkg
    
    * fix lint
    
    * remove unused sdk
    
    * fix lint
    
    * fix test
    
    * fix test
    
    * fix test
    
    * rename network
    
    * rename to blockchain
    
    * refactor sdk path
    
    * update sdk
    
    * update sdk
    
    * update sdk
    
    * update sdk
    
    * update sdk
    
    * address comments
    
    * fix lint
    
    * fix lint
    
    * fix lint
    
    ---------
    
    Signed-off-by: sukantoraymond <rsukanto@umich.edu>

--- pkg/vm/evm_settings.go
+++ pkg/vm/evm_settings.go
@@ -6,13 +6,10 @@ package vm
 import (
 	"math/big"
 
-	"github.com/ava-labs/subnet-evm/commontype"
 	"github.com/ethereum/go-ethereum/common"
 )
 
 var (
-	Difficulty = big.NewInt(0)
-
 	// current avacloud settings
 	LowGasLimit     = big.NewInt(12_000_000)
 	MediumGasLimit  = big.NewInt(15_000_000) // C-Chain value
@@ -22,18 +19,6 @@ var (
 	HighTargetGas   = big.NewInt(60_000_000) // 3x of gas limit
 
 	NoDynamicFeesGasLimitToTargetGasFactor = big.NewInt(5)
-
-	// This is the current c-chain gas config
-	StarterFeeConfig = commontype.FeeConfig{
-		GasLimit:                 big.NewInt(15_000_000),
-		MinBaseFee:               big.NewInt(25_000_000_000),
-		TargetGas:                big.NewInt(15_000_000),
-		BaseFeeChangeDenominator: big.NewInt(36),
-		MinBlockGasCost:          big.NewInt(0),
-		MaxBlockGasCost:          big.NewInt(1_000_000),
-		TargetBlockRate:          2,
-		BlockGasCostStep:         big.NewInt(200_000),
-	}
 
 	PrefundedEwoqAddress = common.HexToAddress("0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC")
 	PrefundedEwoqPrivate = "56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027"
---
+++ sdk/key/key_test.go
@@ -0,0 +1,115 @@
+// Copyright (C) 2019-2022, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package key
+
+import (
+	"bytes"
+	"errors"
+	"path/filepath"
+	"testing"
+
+	"github.com/ava-labs/avalanchego/utils/cb58"
+	"github.com/ava-labs/avalanchego/utils/crypto/secp256k1"
+)
+
+const ewoqPChainAddr = "P-custom18jma8ppw3nhx5r4ap8clazz0dps7rv5u9xde7p"
+
+func TestNewKeyEwoq(t *testing.T) {
+	t.Parallel()
+
+	m, err := NewSoft(
+		WithPrivateKeyEncoded(EwoqPrivateKey),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pAddr, err := m.P("custom")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if pAddr != ewoqPChainAddr {
+		t.Fatalf("unexpected P-Chain address %q, expected %q", pAddr, ewoqPChainAddr)
+	}
+
+	keyPath := filepath.Join(t.TempDir(), "key.pk")
+	if err := m.Save(keyPath); err != nil {
+		t.Fatal(err)
+	}
+
+	m2, err := LoadSoft(keyPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !bytes.Equal(m.PrivKeyRaw(), m2.PrivKeyRaw()) {
+		t.Fatalf("loaded key unexpected %v, expected %v", m2.PrivKeyRaw(), m.PrivKeyRaw())
+	}
+}
+
+func TestNewKey(t *testing.T) {
+	t.Parallel()
+
+	skBytes, err := cb58.Decode(rawEwoqPk)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ewoqPk, err := secp256k1.ToPrivateKey(skBytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	privKey2, err := secp256k1.NewPrivateKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tt := []struct {
+		name   string
+		opts   []SOpOption
+		expErr error
+	}{
+		{
+			name:   "test",
+			opts:   nil,
+			expErr: nil,
+		},
+		{
+			name: "ewop with WithPrivateKey",
+			opts: []SOpOption{
+				WithPrivateKey(ewoqPk),
+			},
+			expErr: nil,
+		},
+		{
+			name: "ewop with WithPrivateKeyEncoded",
+			opts: []SOpOption{
+				WithPrivateKeyEncoded(EwoqPrivateKey),
+			},
+			expErr: nil,
+		},
+		{
+			name: "ewop with WithPrivateKey/WithPrivateKeyEncoded",
+			opts: []SOpOption{
+				WithPrivateKey(ewoqPk),
+				WithPrivateKeyEncoded(EwoqPrivateKey),
+			},
+			expErr: nil,
+		},
+		{
+			name: "ewop with invalid WithPrivateKey",
+			opts: []SOpOption{
+				WithPrivateKey(privKey2),
+				WithPrivateKeyEncoded(EwoqPrivateKey),
+			},
+			expErr: ErrInvalidPrivateKey,
+		},
+	}
+	for i, tv := range tt {
+		_, err := NewSoft(tv.opts...)
+		if !errors.Is(err, tv.expErr) {
+			t.Fatalf("#%d(%s): unexpected error %v, expected %v", i, tv.name, err, tv.expErr)
+		}
+	}
+}
