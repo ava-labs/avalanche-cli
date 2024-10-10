afb44a14f152a58f6354b976ef161eae9e7123fd
Artur Reznikov <artur.reznikov@avalabs.org>
2024-10-10 14:06:54 -0700

    proper etna devnet boostrappers

--- pkg/constants/etna.go
+++ pkg/constants/etna.go
@@ -24,11 +24,6 @@ var (
 		"NodeID-8LbTmmGsDC991SbD8Nkx88VULT3XYzYXC",
 		"NodeID-DDhXtFm6Q9tCq2yiFRmcSMKvHgUgh8yQC",
 		"NodeID-QDYnWDQd6g4cQ5H6yiWNqSmfRMBqEH9AG",
-		"NodeID-P5QGH4EXddrcyNAzkqyZKHXgEpVX6HExL",
-		"NodeID-78ibWpjtZz5ZGT6EyTEdu8VKmboUHTuGT",
-		"NodeID-7eRvnfs2a2PvrPHUuCRRpPVAoVjbWxaFG",
-		"NodeID-gpXWBExQSZXqJPQt6L6MnveUfgr7HJ4q",
-		"NodeID-L4CY8B5uVSDe4cnN1BpeDsHacMp4q4q8q",
 	}
 	EtnaDevnetBootstrapIPs = []string{
 		"107.21.11.213:9651",
@@ -36,10 +31,5 @@ var (
 		"52.201.126.172:9651",
 		"35.170.144.5:9651",
 		"98.82.41.186:9651",
-		"34.228.34.127:9651",
-		"44.205.136.166:9651",
-		"52.6.31.40:9651",
-		"54.197.98.148:9651",
-		"18.211.108.228:9651",
 	}
 )
