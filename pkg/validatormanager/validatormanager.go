5a1a67e00d844f19a43b49ac067b5df4fa690419
Raymond Sukanto <rsukanto@umich.edu>
2024-10-29 11:50:46 -0400

    remove duplicate code

--- pkg/validatormanager/validatormanager.go
+++ pkg/validatormanager/validatormanager.go
@@ -9,20 +9,11 @@ import (
 	"math/big"
 	"strings"
 
-	"github.com/ava-labs/avalanche-cli/pkg/contract"
 	"github.com/ava-labs/avalanche-cli/pkg/models"
 	blockchainSDK "github.com/ava-labs/avalanche-cli/sdk/blockchain"
-	"github.com/ava-labs/avalanche-cli/sdk/interchain"
 	"github.com/ava-labs/avalanchego/api/info"
-	"github.com/ava-labs/avalanchego/ids"
-	avagoconstants "github.com/ava-labs/avalanchego/utils/constants"
 	"github.com/ava-labs/avalanchego/utils/logging"
-	"github.com/ava-labs/avalanchego/vms/platformvm/txs"
-	warp "github.com/ava-labs/avalanchego/vms/platformvm/warp"
-	warpMessage "github.com/ava-labs/avalanchego/vms/platformvm/warp/message"
-	warpPayload "github.com/ava-labs/avalanchego/vms/platformvm/warp/payload"
 	"github.com/ava-labs/subnet-evm/core"
-	"github.com/ava-labs/subnet-evm/core/types"
 	"github.com/ethereum/go-ethereum/common"
 )
 
@@ -80,161 +71,6 @@ func AddPoAValidatorManagerContractToAllocations(
 		Code:    deployedPoaValidatorManagerBytes,
 		Nonce:   1,
 	}
-}
-
-// initializes contract [managerAddress] at [rpcURL], to
-// manage validators on [subnetID], with
-// owner given by [ownerAddress]
-func PoAValidatorManagerInitialize(
-	rpcURL string,
-	managerAddress common.Address,
-	privateKey string,
-	subnetID ids.ID,
-	ownerAddress common.Address,
-) (*types.Transaction, *types.Receipt, error) {
-	const (
-		defaultChurnPeriodSeconds     = uint64(0)
-		defaultMaximumChurnPercentage = uint8(20)
-	)
-	type Params struct {
-		SubnetID               [32]byte
-		ChurnPeriodSeconds     uint64
-		MaximumChurnPercentage uint8
-	}
-	params := Params{
-		SubnetID:               subnetID,
-		ChurnPeriodSeconds:     defaultChurnPeriodSeconds,
-		MaximumChurnPercentage: defaultMaximumChurnPercentage,
-	}
-	return contract.TxToMethod(
-		rpcURL,
-		privateKey,
-		managerAddress,
-		nil,
-		"initialize PoA manager",
-		errorSignatureToError,
-		"initialize((bytes32,uint64,uint8),address)",
-		params,
-		ownerAddress,
-	)
-}
-
-// constructs p-chain-validated (signed) subnet conversion warp
-// message, to be sent to the validators manager when
-// initializing validators set
-// the message specifies [subnetID] that is being converted
-// together with the validator's manager [managerBlockchainID],
-// [managerAddress], and the initial list of [validators]
-func PoaValidatorManagerGetPChainSubnetConversionWarpMessage(
-	network models.Network,
-	aggregatorLogLevel logging.Level,
-	aggregatorQuorumPercentage uint64,
-	aggregatorExtraPeerEndpoints []info.Peer,
-	subnetID ids.ID,
-	managerBlockchainID ids.ID,
-	managerAddress common.Address,
-	convertSubnetValidators []*txs.ConvertSubnetValidator,
-) (*warp.Message, error) {
-	validators := []warpMessage.SubnetConversionValidatorData{}
-	for _, convertSubnetValidator := range convertSubnetValidators {
-		validators = append(validators, warpMessage.SubnetConversionValidatorData{
-			NodeID:       convertSubnetValidator.NodeID[:],
-			BLSPublicKey: convertSubnetValidator.Signer.PublicKey,
-			Weight:       convertSubnetValidator.Weight,
-		})
-	}
-	subnetConversionData := warpMessage.SubnetConversionData{
-		SubnetID:       subnetID,
-		ManagerChainID: managerBlockchainID,
-		ManagerAddress: managerAddress.Bytes(),
-		Validators:     validators,
-	}
-	subnetConversionID, err := warpMessage.SubnetConversionID(subnetConversionData)
-	if err != nil {
-		return nil, err
-	}
-	addressedCallPayload, err := warpMessage.NewSubnetConversion(subnetConversionID)
-	if err != nil {
-		return nil, err
-	}
-	subnetConversionAddressedCall, err := warpPayload.NewAddressedCall(
-		nil,
-		addressedCallPayload.Bytes(),
-	)
-	if err != nil {
-		return nil, err
-	}
-	subnetConversionUnsignedMessage, err := warp.NewUnsignedMessage(
-		network.ID,
-		avagoconstants.PlatformChainID,
-		subnetConversionAddressedCall.Bytes(),
-	)
-	if err != nil {
-		return nil, err
-	}
-	signatureAggregator, err := interchain.NewSignatureAggregator(
-		network,
-		aggregatorLogLevel,
-		subnetID,
-		aggregatorQuorumPercentage,
-		aggregatorExtraPeerEndpoints,
-	)
-	if err != nil {
-		return nil, err
-	}
-	return signatureAggregator.Sign(subnetConversionUnsignedMessage, subnetID[:])
-}
-
-// calls poa manager validators set init method,
-// passing to it the p-chain signed [subnetConversionSignedMessage]
-// so as to verify p-chain already proceesed the associated
-// ConvertSubnetTx
-func PoAValidatorManagerInitializeValidatorsSet(
-	rpcURL string,
-	managerAddress common.Address,
-	privateKey string,
-	subnetID ids.ID,
-	managerBlockchainID ids.ID,
-	convertSubnetValidators []*txs.ConvertSubnetValidator,
-	subnetConversionSignedMessage *warp.Message,
-) (*types.Transaction, *types.Receipt, error) {
-	type InitialValidator struct {
-		NodeID       []byte
-		BlsPublicKey []byte
-		Weight       uint64
-	}
-	type SubnetConversionData struct {
-		SubnetID                     [32]byte
-		ValidatorManagerBlockchainID [32]byte
-		ValidatorManagerAddress      common.Address
-		InitialValidators            []InitialValidator
-	}
-	validators := []InitialValidator{}
-	for _, convertSubnetValidator := range convertSubnetValidators {
-		validators = append(validators, InitialValidator{
-			NodeID:       convertSubnetValidator.NodeID[:],
-			BlsPublicKey: convertSubnetValidator.Signer.PublicKey[:],
-			Weight:       convertSubnetValidator.Weight,
-		})
-	}
-	subnetConversionData := SubnetConversionData{
-		SubnetID:                     subnetID,
-		ValidatorManagerBlockchainID: managerBlockchainID,
-		ValidatorManagerAddress:      managerAddress,
-		InitialValidators:            validators,
-	}
-	return contract.TxToMethodWithWarpMessage(
-		rpcURL,
-		privateKey,
-		managerAddress,
-		subnetConversionSignedMessage,
-		big.NewInt(0),
-		"initialize validator set",
-		errorSignatureToError,
-		"initializeValidatorSet((bytes32,bytes32,address,[(bytes,bytes,uint64)]),uint32)",
-		subnetConversionData,
-		uint32(0),
-	)
 }
 
 // setups PoA manager after a successful execution of
