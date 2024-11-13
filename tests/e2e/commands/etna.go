73069dc2183c06768d0c63ff99f543cf72bef4cb
arturrez <artur.reznikov@avalabs.org>
2024-11-12 17:50:24 -0800

    use 60s staking period

--- tests/e2e/commands/etna.go
+++ tests/e2e/commands/etna.go
@@ -258,7 +258,7 @@ func AddEtnaSubnetValidatorToCluster(
 		"--delegation-fee",
 		"100",
 		"--staking-period",
-		"100s",
+		"60s",
 		"--"+constants.SkipUpdateFlag,
 	)
 	output, err := cmd.CombinedOutput()
