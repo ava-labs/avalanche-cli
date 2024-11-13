cd80967d08dd2db48438f4779c754ff31ef92f41
arturrez <artur.reznikov@avalabs.org>
2024-11-12 16:45:32 -0800

    remove pos non bootstrap validator e2e

--- tests/e2e/testcases/subnet/sov/addRemoveValidatorPoS/suite.go
+++ tests/e2e/testcases/subnet/sov/addRemoveValidatorPoS/suite.go
@@ -128,18 +128,16 @@ var _ = ginkgo.Describe("[Etna AddRemove Validator SOV PoS]", func() {
 		fmt.Println(output)
 	})
 
-	/*
-		ginkgo.It("Can remove non-bootstrap validator", func() {
-			output, err := commands.RemoveEtnaSubnetValidatorFromCluster(
-				testLocalNodeName,
-				subnetName,
-				"http://127.0.0.1:9660",
-				keyName,
-			)
-			gomega.Expect(err).Should(gomega.BeNil())
-			fmt.Println(output)
-		})
-	*/
+	ginkgo.It("Can remove non-bootstrap validator", func() {
+		output, err := commands.RemoveEtnaSubnetValidatorFromCluster(
+			testLocalNodeName,
+			subnetName,
+			"http://127.0.0.1:9660",
+			keyName,
+		)
+		gomega.Expect(err).Should(gomega.BeNil())
+		fmt.Println(output)
+	})
 
 	ginkgo.It("Can destroy local node", func() {
 		output, err := commands.DestroyLocalNode(testLocalNodeName)
