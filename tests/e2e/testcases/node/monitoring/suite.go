33fced1e955a4ba2543642f3dffd931be3ef2748
Raymond Sukanto <rsukanto@umich.edu>
2024-11-15 12:13:36 -0500

    update e2e

--- tests/e2e/testcases/node/monitoring/suite.go
+++ tests/e2e/testcases/node/monitoring/suite.go
@@ -40,7 +40,7 @@ var (
 
 var _ = ginkgo.Describe("[Node monitoring]", func() {
 	ginkgo.It("can create a node", func() {
-		output := commands.NodeCreate(network, avalanchegoVersion, numNodes, true, 0, commands.ExpectSuccess)
+		output := commands.NodeProvision(network, avalanchegoVersion, numNodes, true, 0, commands.ExpectSuccess)
 		fmt.Println(output)
 		gomega.Expect(output).To(gomega.ContainSubstring("AvalancheGo and Avalanche-CLI installed and node(s) are bootstrapping!"))
 		// parse hostName
