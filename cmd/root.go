b04e1f2343da49245138c2e38d25b2687cbce793
Raymond Sukanto <rsukanto@umich.edu>
2024-11-20 12:48:32 -0500

    cleanup commands

--- cmd/root.go
+++ cmd/root.go
@@ -13,7 +13,6 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/ava-labs/avalanche-cli/cmd/backendcmd"
 	"github.com/ava-labs/avalanche-cli/cmd/blockchaincmd"
 	"github.com/ava-labs/avalanche-cli/cmd/configcmd"
 	"github.com/ava-labs/avalanche-cli/cmd/contractcmd"
@@ -83,9 +82,6 @@ in with avalanche subnet create myNewSubnet.`,
 	rootCmd.AddCommand(primarycmd.NewCmd(app))
 	rootCmd.AddCommand(networkcmd.NewCmd(app))
 	rootCmd.AddCommand(keycmd.NewCmd(app))
-
-	// add hidden backend command
-	rootCmd.AddCommand(backendcmd.NewCmd(app))
 
 	// add transaction command
 	rootCmd.AddCommand(transactioncmd.NewCmd(app))
