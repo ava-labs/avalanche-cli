45c9b7eb641beb3cb455262a421173985cd72354
Raymond Sukanto <rsukanto@umich.edu>
2024-12-02 18:14:42 -0500

    change deploy flags

--- cmd/blockchaincmd/deploy.go
+++ cmd/blockchaincmd/deploy.go
@@ -181,7 +181,7 @@ so you can take your locally tested Subnet and deploy it on Fuji or Mainnet.`,
 	cmd.Flags().StringSliceVar(&aggregatorExtraEndpoints, "aggregator-extra-endpoints", nil, "endpoints for extra nodes that are needed in signature aggregation")
 	cmd.Flags().BoolVar(&useLocalMachine, "use-local-machine", false, "use local machine as a blockchain validator")
 	cmd.Flags().IntVar(&numBootstrapValidators, "num-bootstrap-validators", 0, "(only if --generate-node-id is true) number of bootstrap validators to set up in sovereign L1 validator)")
-	cmd.Flags().IntVar(&numLocalNodes, "num-local-nodes", 1, "number of nodes to be created on local machine")
+	cmd.Flags().IntVar(&numLocalNodes, "num-local-nodes", 0, "number of nodes to be created on local machine")
 	cmd.Flags().StringVar(&changeOwnerAddress, "change-owner-address", "", "address that will receive change if node is no longer L1 validator")
 
 	cmd.Flags().Uint64Var(&poSMinimumStakeAmount, "pos-minimum-stake-amount", 1, "minimum stake amount")
@@ -615,6 +615,9 @@ func deployBlockchain(cmd *cobra.Command, args []string) error {
 					network = models.ConvertClusterToNetwork(network)
 				}
 			}
+			if numLocalNodes > 0 {
+				useLocalMachine = true
+			}
 			// ask user if we want to use local machine if cluster is not provided
 			if network.Kind != models.Local && !useLocalMachine && clusterNameFlagValue == "" {
 				ux.Logger.PrintToUser("You can use your local machine as a bootstrap validator on the blockchain")
@@ -624,6 +627,11 @@ func deployBlockchain(cmd *cobra.Command, args []string) error {
 				if err != nil {
 					return err
 				}
+			}
+			// default number of local machine nodes to be 1
+			// we set it here instead of at flag level so that we don't prompt if user wants to use local machine when they set numLocalNodes flag value
+			if useLocalMachine && numLocalNodes == 0 {
+				numLocalNodes = 1
 			}
 			// if no cluster provided - we create one  with fmt.Sprintf("%s-local-node", blockchainName) name
 			if useLocalMachine && clusterNameFlagValue == "" {
