2fc67ff9568541be69f50eb840bc8621fff5493b
arturrez <artur.reznikov@avalabs.org>
2024-12-11 12:01:42 -0800

    rm debug

--- pkg/validatormanager/validatormanager.go
+++ pkg/validatormanager/validatormanager.go
@@ -8,7 +8,6 @@ import (
 	"strings"
 
 	"github.com/ava-labs/avalanche-cli/pkg/models"
-	"github.com/ava-labs/avalanche-cli/pkg/ux"
 	blockchainSDK "github.com/ava-labs/avalanche-cli/sdk/blockchain"
 	validatorManagerSDK "github.com/ava-labs/avalanche-cli/sdk/validatormanager"
 	"github.com/ava-labs/avalanchego/api/info"
@@ -91,7 +90,6 @@ func AddRewardCalculatorToAllocations(
 	allocs core.GenesisAlloc,
 	rewardBasisPoints uint64,
 ) {
-	ux.Logger.PrintToUser("Adding reward calculator contract to allocations %d", rewardBasisPoints)
 	deployedRewardCalculatorBytes := common.FromHex(strings.TrimSpace(string(deployedRewardCalculatorBytecode)))
 	allocs[common.HexToAddress(validatorManagerSDK.RewardCalculatorAddress)] = core.GenesisAccount{
 		Balance: big.NewInt(0),
