2b316ed2465db30d596a2d825b8aeb24c696519f
Felipe Madero <felipe.madero@gmail.com>
2024-12-13 12:54:55 -0300

    use cluster uris

--- tests/e2e/testcases/subnet/sov/addRemoveValidatorPoA/suite.go
+++ tests/e2e/testcases/subnet/sov/addRemoveValidatorPoA/suite.go
@@ -5,14 +5,14 @@ package subnet
 
 import (
 	"fmt"
+	"net/url"
 	"regexp"
 
 	"github.com/ava-labs/avalanche-cli/tests/e2e/commands"
+	"github.com/ava-labs/avalanche-cli/tests/e2e/utils"
 	ginkgo "github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 )
-
-var blockchainID = ""
 
 const (
 	subnetName        = "e2eSubnetTest"
@@ -20,6 +20,11 @@ const (
 	ewoqEVMAddress    = "0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC"
 	ewoqPChainAddress = "P-custom18jma8ppw3nhx5r4ap8clazz0dps7rv5u9xde7p"
 	testLocalNodeName = "e2eSubnetTest-local-node"
+)
+
+var (
+	blockchainID     string
+	localClusterUris []string
 )
 
 var _ = ginkgo.Describe("[Etna AddRemove Validator SOV PoA]", func() {
@@ -43,6 +48,9 @@ var _ = ginkgo.Describe("[Etna AddRemove Validator SOV PoA]", func() {
 		)
 		gomega.Expect(err).Should(gomega.BeNil())
 		fmt.Println(output)
+		localClusterUris, err = utils.GetLocalClusterUris()
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(len(localClusterUris)).Should(gomega.Equal(7))
 	})
 
 	ginkgo.It("Deploy Etna Subnet", func() {
@@ -50,11 +58,11 @@ var _ = ginkgo.Describe("[Etna AddRemove Validator SOV PoA]", func() {
 			subnetName,
 			testLocalNodeName,
 			[]string{
-				"http://127.0.0.1:9650",
-				"http://127.0.0.1:9652",
-				"http://127.0.0.1:9654",
-				"http://127.0.0.1:9656",
-				"http://127.0.0.1:9658",
+				localClusterUris[0],
+				localClusterUris[1],
+				localClusterUris[2],
+				localClusterUris[3],
+				localClusterUris[4],
 			},
 			ewoqPChainAddress,
 			true, // convertOnly
@@ -83,7 +91,7 @@ var _ = ginkgo.Describe("[Etna AddRemove Validator SOV PoA]", func() {
 	ginkgo.It("Can initialize a PoA Manager contract", func() {
 		output, err := commands.InitValidatorManager(subnetName,
 			testLocalNodeName,
-			"http://127.0.0.1:9650",
+			localClusterUris[0],
 			blockchainID,
 		)
 		gomega.Expect(err).Should(gomega.BeNil())
@@ -94,7 +102,7 @@ var _ = ginkgo.Describe("[Etna AddRemove Validator SOV PoA]", func() {
 		output, err := commands.AddEtnaSubnetValidatorToCluster(
 			testLocalNodeName,
 			subnetName,
-			"http://127.0.0.1:9660",
+			localClusterUris[5],
 			ewoqPChainAddress,
 			1,
 			false, // use existing avago running
@@ -107,7 +115,7 @@ var _ = ginkgo.Describe("[Etna AddRemove Validator SOV PoA]", func() {
 		output, err := commands.AddEtnaSubnetValidatorToCluster(
 			testLocalNodeName,
 			subnetName,
-			"http://127.0.0.1:9662",
+			localClusterUris[6],
 			ewoqPChainAddress,
 			1,
 			false, // use existing avago running
@@ -120,17 +128,26 @@ var _ = ginkgo.Describe("[Etna AddRemove Validator SOV PoA]", func() {
 		output, err := commands.GetLocalClusterStatus(testLocalNodeName, subnetName)
 		gomega.Expect(err).Should(gomega.BeNil())
 		fmt.Println(output)
-		// make sure we can find string with "http://127.0.0.1:9660" and "L1:Validating" string in the output
-		gomega.Expect(output).To(gomega.MatchRegexp(`http://127\.0\.0\.1:9652.*Validating`), "expect to have L1 validating")
-		// make sure we can do the same for "http://127.0.0.1:9662"
-		gomega.Expect(output).To(gomega.MatchRegexp(`http://127\.0\.0\.1:9654.*Validating`), "expect to have L1 validating")
+		// make sure we can find string with "http://127.0.0.1:port" and "L1:Validating" string in the output
+		parsedURL, err := url.Parse(localClusterUris[1])
+		gomega.Expect(err).Should(gomega.BeNil())
+		port := parsedURL.Port()
+		gomega.Expect(port).Should(gomega.Not(gomega.BeEmpty()))
+		regexp := fmt.Sprintf(`http://127\.0\.0\.1:%s.*Validating`, port)
+		gomega.Expect(output).To(gomega.MatchRegexp(regexp), fmt.Sprintf("expect to have L1 validated by port %s", port))
+		parsedURL, err = url.Parse(localClusterUris[2])
+		gomega.Expect(err).Should(gomega.BeNil())
+		port = parsedURL.Port()
+		gomega.Expect(port).Should(gomega.Not(gomega.BeEmpty()))
+		regexp = fmt.Sprintf(`http://127\.0\.0\.1:%s.*Validating`, port)
+		gomega.Expect(output).To(gomega.MatchRegexp(regexp), fmt.Sprintf("expect to have L1 validated by port %s", port))
 	})
 
 	ginkgo.It("Can remove bootstrap validator", func() {
 		output, err := commands.RemoveEtnaSubnetValidatorFromCluster(
 			testLocalNodeName,
 			subnetName,
-			"http://127.0.0.1:9654",
+			localClusterUris[2],
 			keyName,
 		)
 		gomega.Expect(err).Should(gomega.BeNil())
@@ -141,7 +158,7 @@ var _ = ginkgo.Describe("[Etna AddRemove Validator SOV PoA]", func() {
 		output, err := commands.RemoveEtnaSubnetValidatorFromCluster(
 			testLocalNodeName,
 			subnetName,
-			"http://127.0.0.1:9660",
+			localClusterUris[5],
 			keyName,
 		)
 		gomega.Expect(err).Should(gomega.BeNil())
