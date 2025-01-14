ba9ad16200a5d554a36cfda7b0fb6c4ff98e2adc
Raymond Sukanto <rsukanto@umich.edu>
2025-01-14 18:06:20 +0700

    lint

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -58,7 +58,7 @@ const (
 	APIRequestLargeTimeout = 10 * time.Second
 	FastGRPCDialTimeout    = 100 * time.Millisecond
 
-	FujiBootstrapTimeout    = 30 * time.Minute
+	FujiBootstrapTimeout    = 15 * time.Minute
 	MainnetBootstrapTimeout = 24 * time.Hour
 
 	SSHServerStartTimeout       = 1 * time.Minute
