e16a7736a811688e8349e609fc8e6a0de352279b
cuiweiyuan <cuiweiyuan@aliyun.com>
2025-01-15 23:24:37 +0800

    chore: fix some function names in comment

--- pkg/utils/common.go
+++ pkg/utils/common.go
@@ -261,7 +261,7 @@ func Unique(slice []string) []string {
 	return uniqueSlice
 }
 
-// containsIgnoreCase checks if the given string contains the specified substring, ignoring case.
+// ContainsIgnoreCase checks if the given string contains the specified substring, ignoring case.
 func ContainsIgnoreCase(s, substr string) bool {
 	return strings.Contains(strings.ToLower(s), strings.ToLower(substr))
 }
