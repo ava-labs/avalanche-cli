198e2308b936eeba847578b0eecf1b9b5f7c27a1
Raymond Sukanto <rsukanto@umich.edu>
2025-01-30 16:34:58 -0500

    fix e2e

--- tests/e2e/testcases/network/suite.go
+++ tests/e2e/testcases/network/suite.go
@@ -82,8 +82,7 @@ var _ = ginkgo.Describe("[Network]", ginkgo.Ordered, func() {
 
 	ginkgo.It("can stop and restart a deployed subnet SOV", func() {
 		commands.CreateSubnetEvmConfigSOV(subnetName, utils.SubnetEvmGenesisPoaPath)
-		// TODO: use commands.DeploySubnetLocallySOV once having etna release
-		deployOutput := commands.DeploySubnetLocallyWithVersionSOV(subnetName, utils.EtnaAvalancheGoVersion)
+		deployOutput := commands.DeploySubnetLocallySOV(subnetName)
 		rpcs, err := utils.ParseRPCsFromOutput(deployOutput)
 		if err != nil {
 			fmt.Println(deployOutput)
