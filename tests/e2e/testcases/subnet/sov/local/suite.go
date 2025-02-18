e5e3fdc8b89a460450c46e971a412531686ccafd
Felipe Madero <felipe.madero@gmail.com>
2025-02-18 11:25:47 -0300

    e2e fix

--- tests/e2e/testcases/subnet/sov/local/suite.go
+++ tests/e2e/testcases/subnet/sov/local/suite.go
@@ -224,20 +224,17 @@ var _ = ginkgo.Describe("[Local Subnet SOV]", ginkgo.Ordered, func() {
 
 		// create per node chain config
 		nodesRPCTxFeeCap := map[string]string{
-			"node1": "101",
-			"node2": "102",
-			"node3": "103",
-			"node4": "104",
-			"node5": "105",
+			"NodeID-7Xhw2mDxuDS44j42TCB6U5579esbSt3Lg": "101",
+			"NodeID-MFrZFVCXPv5iCn6M9K6XduxGTYp891xXZ": "102",
 		}
 		perNodeChainConfig := "{\n"
 		i := 0
-		for nodeName, rpcTxFeeCap := range nodesRPCTxFeeCap {
+		for nodeID, rpcTxFeeCap := range nodesRPCTxFeeCap {
 			commaStr := ","
 			if i == len(nodesRPCTxFeeCap)-1 {
 				commaStr = ""
 			}
-			perNodeChainConfig += fmt.Sprintf("  \"%s\": {\"rpc-tx-fee-cap\": %s}%s\n", nodeName, rpcTxFeeCap, commaStr)
+			perNodeChainConfig += fmt.Sprintf("  \"%s\": {\"rpc-tx-fee-cap\": %s}%s\n", nodeID, rpcTxFeeCap, commaStr)
 			i++
 		}
 		perNodeChainConfig += "}\n"
@@ -266,11 +263,11 @@ var _ = ginkgo.Describe("[Local Subnet SOV]", ginkgo.Ordered, func() {
 		// verify that plugin logs reflect per node configuration
 		nodesInfo, err := utils.GetNodesInfo()
 		gomega.Expect(err).Should(gomega.BeNil())
-		for nodeName, nodeInfo := range nodesInfo {
+		for nodeID, nodeInfo := range nodesInfo {
 			logFile := path.Join(nodeInfo.LogDir, blockchainID+".log")
 			fileBytes, err := os.ReadFile(logFile)
 			gomega.Expect(err).Should(gomega.BeNil())
-			rpcTxFeeCap, ok := nodesRPCTxFeeCap[nodeName]
+			rpcTxFeeCap, ok := nodesRPCTxFeeCap[nodeID]
 			gomega.Expect(ok).Should(gomega.BeTrue())
 			gomega.Expect(fileBytes).Should(gomega.ContainSubstring("RPCTxFeeCap:%s", rpcTxFeeCap))
 		}
