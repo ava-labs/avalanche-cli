a5d67892585fba1b75f729c1389c2617fce6e596
Raymond Sukanto <rsukanto@umich.edu>
2025-03-18 19:16:00 -0400

    lint

--- cmd/flags/validator_manager.go
+++ cmd/flags/validator_manager.go
@@ -8,11 +8,9 @@ import (
 )
 
 const (
-	rpcURLFLag                      = "rpc"
-	aggregatorLogLevelFlag          = "aggregator-log-level"
-	aggregatorLogToStdoutFlag       = "aggregator-log-to-stdout"
-	aggregatorExtraEndpointsFlag    = "aggregator-extra-endpoints"
-	aggregatorAllowPrivatePeersFlag = "aggregator-allow-private-peers"
+	rpcURLFLag                = "rpc"
+	aggregatorLogLevelFlag    = "aggregator-log-level"
+	aggregatorLogToStdoutFlag = "aggregator-log-to-stdout"
 )
 
 type ValidatorManagerFlags struct {
@@ -23,15 +21,11 @@ type ValidatorManagerFlags struct {
 type SignatureAggregatorFlags struct {
 	AggregatorLogLevel    string
 	AggregatorLogToStdout bool
-	//AggregatorExtraEndpoints    []string
-	//AggregatorAllowPrivatePeers bool
 }
 
 func AddValidatorManagerFlagsToCmd(cmd *cobra.Command, flags *ValidatorManagerFlags, addRPCFlag bool) {
 	cmd.Flags().StringVar(&flags.SigAggFlags.AggregatorLogLevel, aggregatorLogLevelFlag, constants.DefaultAggregatorLogLevel, "log level to use with signature aggregator")
 	cmd.Flags().BoolVar(&flags.SigAggFlags.AggregatorLogToStdout, aggregatorLogToStdoutFlag, false, "use stdout for signature aggregator logs")
-	//cmd.Flags().StringSliceVar(&flags.SigAggFlags.AggregatorExtraEndpoints, aggregatorExtraEndpointsFlag, nil, "endpoints for extra nodes that are needed in signature aggregation")
-	//cmd.Flags().BoolVar(&flags.SigAggFlags.AggregatorAllowPrivatePeers, aggregatorAllowPrivatePeersFlag, true, "allow the signature aggregator to connect to peers with private IP")
 	if addRPCFlag {
 		cmd.Flags().StringVar(&flags.RPC, rpcURLFLag, "", "connect to validator manager at the given rpc endpoint")
 	}
