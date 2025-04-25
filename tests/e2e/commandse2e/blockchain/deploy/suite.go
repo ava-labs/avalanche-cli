1e8290a65c2edebe32d0ac1fa67059f956310fac
sukantoraymond <rsukanto@umich.edu>
2025-04-25 12:34:47 -0400

    Core Test Function (#2762)
    * core test function
    
    * lint
    
    * lint

---
+++ tests/e2e/commandse2e/blockchain/deploy/suite.go
@@ -0,0 +1,47 @@
+// Copyright (C) 2024, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package deploy
+
+import (
+	"github.com/ava-labs/avalanche-cli/tests/e2e/commandse2e"
+
+	"github.com/ava-labs/avalanche-cli/tests/e2e/commands"
+	ginkgo "github.com/onsi/ginkgo/v2"
+	"github.com/onsi/gomega"
+)
+
+const (
+	subnetName = "testSubnet"
+)
+
+const ewoqEVMAddress = "0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC"
+
+var _ = ginkgo.Describe("[Blockchain Deploy Flags]", ginkgo.Ordered, func() {
+	_ = ginkgo.BeforeEach(func() {
+		// Create test subnet config
+		commands.CreateEtnaSubnetEvmConfig(subnetName, ewoqEVMAddress, commands.PoA)
+	})
+
+	ginkgo.AfterEach(func() {
+		commands.CleanNetwork()
+		// Cleanup test subnet config
+		commands.DeleteSubnetConfig(subnetName)
+	})
+	blockchainCmdArgs := []string{subnetName}
+	ginkgo.It("HAPPY PATH: local deploy default", func() {
+		testFlags := commandse2e.TestFlags{}
+		output, err := commandse2e.TestCommand(commandse2e.BlockchainCmd, "deploy", blockchainCmdArgs, testFlags)
+		gomega.Expect(output).Should(gomega.ContainSubstring("L1 is successfully deployed on Local Network"))
+		gomega.Expect(err).Should(gomega.BeNil())
+	})
+
+	ginkgo.It("ERROR PATH: invalid_version", func() {
+		testFlags := commandse2e.TestFlags{
+			"avalanchego-version": "invalid_version",
+		}
+		output, err := commandse2e.TestCommand(commandse2e.BlockchainCmd, "deploy", blockchainCmdArgs, testFlags)
+		gomega.Expect(err).Should(gomega.HaveOccurred())
+		gomega.Expect(output).Should(gomega.ContainSubstring("invalid version string"))
+	})
+})
