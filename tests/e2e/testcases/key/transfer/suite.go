d9bb8d7c93aac4a99aedca9988ac21951c5d91b8
artemanosov <aranosov@protonmail.com>
2025-05-14 11:20:22 -0400

    Refactor SendICMMessage util function

--- tests/e2e/testcases/key/transfer/suite.go
+++ tests/e2e/testcases/key/transfer/suite.go
@@ -299,7 +299,8 @@ var _ = ginkgo.Describe("[Key] transfer", func() {
 		ginkgo.It("can transfer from C-Chain to Subnet with ewoq key and local key", func() {
 			commands.CreateSubnetEvmConfigNonSOV(subnetName, utils.SubnetEvmGenesisPath, true)
 			commands.DeploySubnetLocallyNonSOV(subnetName)
-			commands.SendICMMessage("--local", "cchain", subnetName, "hello world", ewoqKeyName)
+			_, err := commands.SendICMMessage([]string{"cchain", subnetName, "hello world"}, utils.TestFlags{"key": ewoqKeyName})
+			gomega.Expect(err).Should(gomega.BeNil())
 			output := commands.DeployERC20Contract("--local", ewoqKeyName, "TEST", "100000", ewoqEVMAddress, "--c-chain")
 			erc20Address, err := utils.GetERC20TokenAddress(output)
 			gomega.Expect(err).Should(gomega.BeNil())
