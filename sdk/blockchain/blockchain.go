59914dd543f2b3ab6aaf2ca39b6bf1326e9bf79f
Raymond Sukanto <rsukanto@umich.edu>
2025-06-03 18:46:01 -0400

    refactor

--- sdk/blockchain/blockchain.go
+++ sdk/blockchain/blockchain.go
@@ -10,9 +10,7 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"math/big"
-	"net/http"
 	"os"
 	"path/filepath"
 	"time"
@@ -30,7 +28,6 @@ import (
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/utils/logging"
 	"github.com/ava-labs/avalanchego/vms/platformvm/txs"
-	"github.com/ava-labs/avalanchego/vms/platformvm/warp"
 	commonAvago "github.com/ava-labs/avalanchego/wallet/subnet/primary/common"
 	"github.com/ava-labs/subnet-evm/commontype"
 	"github.com/ava-labs/subnet-evm/core"
@@ -428,7 +425,7 @@ func (c *Subnet) InitializeProofOfAuthority(
 	chainIDHexStr := hex.EncodeToString(c.SubnetID[:])
 	messageHexStr := hex.EncodeToString(subnetConversionUnsignedMessage.Bytes())
 
-	signedMessage, err := SignMessage(messageHexStr, chainIDHexStr, c.SubnetID.String(), constants.DefaultQuorumPercentage, aggregatorLogger)
+	signedMessage, err := interchain.SignMessage(messageHexStr, chainIDHexStr, c.SubnetID.String(), constants.DefaultQuorumPercentage, aggregatorLogger)
 	if err != nil {
 		return fmt.Errorf("failed to get signed message: %w", err)
 	}
@@ -532,138 +529,4 @@ func (c *Subnet) InitializeProofOfStake(
 		return evm.TransactionError(tx, err, "failure initializing validators set on pos manager")
 	}
 	return nil
-}
-
-type SignatureAggregatorConfig struct {
-	LogLevel             string       `json:"log-level"`
-	PChainAPI            APIConfig    `json:"p-chain-api"`
-	InfoAPI              APIConfig    `json:"info-api"`
-	SignatureCacheSize   int          `json:"signature-cache-size"`
-	AllowPrivateIPs      bool         `json:"allow-private-ips"`
-	TrackedSubnetIDs     []string     `json:"tracked-subnet-ids"`
-	ManuallyTrackedPeers []PeerConfig `json:"manually-tracked-peers"`
-}
-
-type APIConfig struct {
-	BaseURL string `json:"base-url"`
-}
-
-type PeerConfig struct {
-	ID string `json:"id"`
-	IP string `json:"ip"`
-}
-
-func createSignatureAggregatorConfig(subnetID string, networkEndpoint string, peers []info.Peer) *SignatureAggregatorConfig {
-	config := &SignatureAggregatorConfig{
-		LogLevel:             "debug",
-		PChainAPI:            APIConfig{BaseURL: networkEndpoint},
-		InfoAPI:              APIConfig{BaseURL: networkEndpoint},
-		SignatureCacheSize:   1048576,
-		AllowPrivateIPs:      true,
-		TrackedSubnetIDs:     []string{subnetID},
-		ManuallyTrackedPeers: make([]PeerConfig, 0),
-	}
-
-	for _, peer := range peers {
-		// Skip peers with invalid IP addresses
-		if !peer.Info.PublicIP.IsValid() {
-			continue
-		}
-		fmt.Printf("peer.Info.PublicIP.String() %s \n", peer.Info.PublicIP.String())
-		config.ManuallyTrackedPeers = append(config.ManuallyTrackedPeers, PeerConfig{
-			ID: peer.Info.ID.String(),
-			IP: peer.Info.PublicIP.String(),
-		})
-	}
-
-	return config
-}
-
-type AggregateSignaturesRequest struct {
-	Message                string `json:"message"`
-	Justification          string `json:"justification"`
-	SigningSubnetID        string `json:"signing-subnet-id"`
-	QuorumPercentage       int    `json:"quorum-percentage"`
-	QuorumPercentageBuffer int    `json:"quorum-percentage-buffer,omitempty"`
-}
-
-func SignMessage(message, justification, signingSubnetID string, quorumPercentage int, logger logging.Logger) (*warp.Message, error) {
-	request := AggregateSignaturesRequest{
-		Message:          message,
-		Justification:    justification,
-		SigningSubnetID:  signingSubnetID,
-		QuorumPercentage: quorumPercentage,
-	}
-
-	requestBody, err := json.Marshal(request)
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal request: %w", err)
-	}
-
-	logger.Info("Calling signature aggregator",
-		zap.String("request", string(requestBody)),
-	)
-
-	client := &http.Client{
-		Timeout: 30 * time.Second,
-	}
-
-	resp, err := client.Post(
-		"http://localhost:8080/aggregate-signatures",
-		"application/json",
-		bytes.NewBuffer(requestBody),
-	)
-	if err != nil {
-		logger.Error("Error making request to signature aggregator",
-			zap.Error(err),
-		)
-		return nil, fmt.Errorf("failed to make request: %w", err)
-	}
-	defer func() {
-		if closeErr := resp.Body.Close(); closeErr != nil {
-			logger.Warn("Failed to close response body",
-				zap.Error(closeErr),
-			)
-		}
-	}()
-
-	// Read response body
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		logger.Error("Error reading response body",
-			zap.Error(err),
-		)
-		return nil, fmt.Errorf("failed to read response body: %w", err)
-	}
-
-	logger.Info("Received response from signature aggregator",
-		zap.Int("status_code", resp.StatusCode),
-		zap.String("response", string(body)),
-	)
-
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("signature aggregator returned non-200 status code: %d, body: %s", resp.StatusCode, string(body))
-	}
-
-	// Parse the response to get the signed message hex
-	var response struct {
-		SignedMessage string `json:"signed-message"`
-	}
-	if err := json.Unmarshal(body, &response); err != nil {
-		return nil, fmt.Errorf("failed to parse response: %w", err)
-	}
-
-	// Decode the hex string
-	signedMessageBytes, err := hex.DecodeString(response.SignedMessage)
-	if err != nil {
-		return nil, fmt.Errorf("error decoding hex: %w", err)
-	}
-
-	// Parse the signed message
-	signedMessage, err := warp.ParseMessage(signedMessageBytes)
-	if err != nil {
-		return nil, fmt.Errorf("error parsing signed message: %w", err)
-	}
-
-	return signedMessage, nil
 }
--- sdk/interchain/signature-aggregator.go
+++ sdk/interchain/signature-aggregator.go
@@ -3,11 +3,13 @@
 package interchain
 
 import (
+	"bytes"
 	"context"
 	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"os"
@@ -465,4 +467,96 @@ type APIConfig struct {
 type PeerConfig struct {
 	ID string `json:"id"`
 	IP string `json:"ip"`
+}
+
+// AggregateSignaturesRequest represents the request structure for aggregating signatures
+type AggregateSignaturesRequest struct {
+	Message                string `json:"message"`
+	Justification          string `json:"justification"`
+	SigningSubnetID        string `json:"signing-subnet-id"`
+	QuorumPercentage       int    `json:"quorum-percentage"`
+	QuorumPercentageBuffer int    `json:"quorum-percentage-buffer,omitempty"`
+}
+
+// SignMessage sends a request to the signature aggregator to sign a message.
+// It returns the signed warp message or an error if the operation fails.
+func SignMessage(message, justification, signingSubnetID string, quorumPercentage int, logger logging.Logger) (*warp.Message, error) {
+	request := AggregateSignaturesRequest{
+		Message:          message,
+		Justification:    justification,
+		SigningSubnetID:  signingSubnetID,
+		QuorumPercentage: quorumPercentage,
+	}
+
+	requestBody, err := json.Marshal(request)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal request: %w", err)
+	}
+
+	logger.Info("Calling signature aggregator",
+		zap.String("request", string(requestBody)),
+	)
+
+	client := &http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	resp, err := client.Post(
+		"http://localhost:8080/aggregate-signatures",
+		"application/json",
+		bytes.NewBuffer(requestBody),
+	)
+	if err != nil {
+		logger.Error("Error making request to signature aggregator",
+			zap.Error(err),
+		)
+		return nil, fmt.Errorf("failed to make request: %w", err)
+	}
+	defer func() {
+		if closeErr := resp.Body.Close(); closeErr != nil {
+			logger.Warn("Failed to close response body",
+				zap.Error(closeErr),
+			)
+		}
+	}()
+
+	// Read response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logger.Error("Error reading response body",
+			zap.Error(err),
+		)
+		return nil, fmt.Errorf("failed to read response body: %w", err)
+	}
+
+	logger.Info("Received response from signature aggregator",
+		zap.Int("status_code", resp.StatusCode),
+		zap.String("response", string(body)),
+	)
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("signature aggregator returned non-200 status code: %d, body: %s", resp.StatusCode, string(body))
+	}
+
+	// Parse the response to get the signed message hex
+	var response struct {
+		SignedMessage string `json:"signed-message"`
+	}
+	if err := json.Unmarshal(body, &response); err != nil {
+		return nil, fmt.Errorf("failed to parse response: %w", err)
+	}
+
+	// Decode the hex string
+	signedMessageBytes, err := hex.DecodeString(response.SignedMessage)
+	if err != nil {
+		return nil, fmt.Errorf("error decoding hex: %w", err)
+	}
+
+	// Parse the signed message
+	signedMessage, err := warp.ParseMessage(signedMessageBytes)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing signed message: %w", err)
+	}
+
+	return signedMessage, nil
 }
