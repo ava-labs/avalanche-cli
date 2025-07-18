68c727ad86d21848c12940a78a4af42ca5462aea
Tony Zhang <tony.zhang@avalabs.org>
2025-07-18 09:54:52 -0700

    adding more test cases per feedback

--- tests/e2e/testcases/network/clean/suite.go
+++ tests/e2e/testcases/network/clean/suite.go
@@ -4,7 +4,12 @@
 package network
 
 import (
+	"fmt"
+
+	"github.com/ava-labs/avalanche-cli/cmd"
+	"github.com/ava-labs/avalanche-cli/pkg/constants"
 	"github.com/ava-labs/avalanche-cli/tests/e2e/commands"
+	"github.com/ava-labs/avalanche-cli/tests/e2e/utils"
 	ginkgo "github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 )
@@ -20,9 +25,96 @@ var _ = ginkgo.Describe("[Local Network] Clean", ginkgo.Ordered, func() {
 		gomega.Expect(out).Should(gomega.ContainSubstring("Process terminated"))
 	})
 
+	ginkgo.It("can clean a deployed L1", func() {
+		testSubnetName := "testSubnet2"
+		commands.CreateEtnaSubnetEvmConfig(testSubnetName, utils.EwoqEVMAddress, commands.PoA)
+
+		// Deploy a local L1
+		out, err := commands.DeployBlockchain(
+			testSubnetName,
+			utils.TestFlags{
+				"local":             true,
+				"skip-icm-deploy":   true,
+				"skip-update-check": true,
+			},
+		)
+		gomega.Expect(out).Should(gomega.ContainSubstring("L1 is successfully deployed on Local Network"))
+		gomega.Expect(err).Should(gomega.BeNil())
+
+		// Clean the network
+		out, err = commands.CleanNetwork()
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(out).Should(gomega.ContainSubstring("Process terminated"))
+
+		// Check L1 status - should not be running
+		out, err = utils.TestCommand(
+			cmd.BlockchainCmd,
+			"stats",
+			[]string{
+				testSubnetName,
+				"--local",
+				"--" + constants.SkipUpdateFlag,
+			},
+			utils.GlobalFlags{},
+			utils.TestFlags{},
+		)
+		gomega.Expect(err).ShouldNot(gomega.BeNil())
+		gomega.Expect(out).Should(gomega.ContainSubstring("no subnetID found for the provided blockchain name"))
+
+		commands.DeleteSubnetConfig(testSubnetName)
+	})
+
 	ginkgo.It("should err out when no network is running", func() {
 		out, err := commands.CleanNetwork()
 		gomega.Expect(err).Should(gomega.BeNil())
 		gomega.Expect(out).Should(gomega.ContainSubstring("No network is running"))
+	})
+
+	ginkgo.It("should only clean default snapshot", func() {
+		// Start the network
+		out, err := commands.StartNetwork()
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(out).Should(gomega.ContainSubstring("Network ready to use"))
+
+		// Stop the network with default snapshot
+		err = commands.StopNetwork()
+		gomega.Expect(err).Should(gomega.BeNil())
+
+		// check if default snapshot exists
+		snapshotExists := utils.CheckSnapshotExists("default")
+		gomega.Expect(snapshotExists).Should(gomega.BeTrue(),
+			fmt.Sprintf("snapshot %s should exist", "default"))
+
+		// Start the network
+		out, err = commands.StartNetwork()
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(out).Should(gomega.ContainSubstring("Network ready to use"))
+
+		// Stop the network with custome snapshot
+		testSnapshotName := "test-snapshot"
+		err = commands.StopNetwork("--snapshot-name", testSnapshotName)
+		gomega.Expect(err).Should(gomega.BeNil())
+
+		// check if custom snapshot exists
+		snapshotExists = utils.CheckSnapshotExists(testSnapshotName)
+		gomega.Expect(snapshotExists).Should(gomega.BeTrue(),
+			fmt.Sprintf("snapshot %s should exist", testSnapshotName))
+
+		// Clean the network
+		out, err = commands.CleanNetwork()
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(out).Should(gomega.ContainSubstring("No network is running"))
+
+		// default snapshot should be cleaned up
+		snapshotExists = utils.CheckSnapshotExists("default")
+		gomega.Expect(snapshotExists).Should(gomega.BeFalse(),
+			fmt.Sprintf("snapshot %s should not exist", "default"))
+
+		// custom snapshot should still exist
+		snapshotExists = utils.CheckSnapshotExists(testSnapshotName)
+		gomega.Expect(snapshotExists).Should(gomega.BeTrue(),
+			fmt.Sprintf("snapshot %s should exist", testSnapshotName))
+
+		utils.DeleteSnapshot(testSnapshotName)
 	})
 })
--- tests/e2e/testcases/network/start/suite.go
+++ tests/e2e/testcases/network/start/suite.go
@@ -7,8 +7,10 @@ import (
 	"fmt"
 	"strconv"
 
+	"github.com/ava-labs/avalanche-cli/cmd"
 	"github.com/ava-labs/avalanche-cli/pkg/constants"
 	"github.com/ava-labs/avalanche-cli/tests/e2e/commands"
+	"github.com/ava-labs/avalanche-cli/tests/e2e/utils"
 	ginkgo "github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 )
@@ -61,5 +63,128 @@ var _ = ginkgo.Describe("[Local Network] Start", ginkgo.Ordered, func() {
 		out, err = commands.GetNetworkStatus()
 		gomega.Expect(err).Should(gomega.BeNil())
 		gomega.Expect(out).Should(gomega.ContainSubstring("Network is Up"))
+	})
+
+	ginkgo.It("can start stopped network with preserved state - default snapshot", func() {
+		// start network with given number of nodes
+		numOfNodes := uint(5)
+		out, err := commands.StartNetworkWithParams(map[string]interface{}{
+			"num-nodes": strconv.FormatUint(uint64(numOfNodes), 10),
+		})
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(out).Should(gomega.ContainSubstring("Network ready to use"))
+
+		// check network status - number of nodes
+		out, err = commands.GetNetworkStatus()
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(out).Should(gomega.ContainSubstring("Network is Up"))
+		gomega.Expect(out).Should(gomega.ContainSubstring(fmt.Sprintf("Number of Nodes: %d", numOfNodes)))
+
+		// stop the network
+		err = commands.StopNetwork()
+		gomega.Expect(err).Should(gomega.BeNil())
+
+		// now start the network again with no arguments
+		out, err = commands.StartNetwork()
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(out).Should(gomega.ContainSubstring("Network ready to use"))
+
+		// check network status - number of nodes should be preserved
+		out, err = commands.GetNetworkStatus()
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(out).Should(gomega.ContainSubstring("Network is Up"))
+		gomega.Expect(out).Should(gomega.ContainSubstring(fmt.Sprintf("Number of Nodes: %d", numOfNodes)))
+	})
+
+	ginkgo.It("can start properly with given snapshot - custom snapshot", func() {
+		// start network with given number of nodes
+		numOfNodes := uint(5)
+		out, err := commands.StartNetworkWithParams(map[string]interface{}{
+			"num-nodes": strconv.FormatUint(uint64(numOfNodes), 10),
+		})
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(out).Should(gomega.ContainSubstring("Network ready to use"))
+
+		// stop with snapshot name
+		testSnapshotName := "test-snapshot"
+		err = commands.StopNetwork("--snapshot-name", testSnapshotName)
+		gomega.Expect(err).Should(gomega.BeNil())
+
+		// check snapshot exists
+		snapshotExists := utils.CheckSnapshotExists(testSnapshotName)
+		gomega.Expect(snapshotExists).Should(gomega.BeTrue(),
+			fmt.Sprintf("snapshot %s should exist", testSnapshotName))
+
+		// start the network with snapshot name
+		out, err = commands.StartNetworkWithParams(map[string]interface{}{
+			"snapshot-name": testSnapshotName,
+		})
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(out).Should(gomega.ContainSubstring("Network ready to use"))
+
+		// check network status - number of nodes should be preserved
+		out, err = commands.GetNetworkStatus()
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(out).Should(gomega.ContainSubstring("Network is Up"))
+		gomega.Expect(out).Should(gomega.ContainSubstring(fmt.Sprintf("Number of Nodes: %d", numOfNodes)))
+
+		utils.DeleteSnapshot(testSnapshotName)
+	})
+
+	ginkgo.It("can start a deployed but stopped L1", func() {
+		testSubnetName := "testSubnet1"
+		commands.CreateEtnaSubnetEvmConfig(testSubnetName, utils.EwoqEVMAddress, commands.PoA)
+
+		// Deploy a local L1
+		out, err := commands.DeployBlockchain(
+			testSubnetName,
+			utils.TestFlags{
+				"local":             true,
+				"skip-icm-deploy":   true,
+				"skip-update-check": true,
+			},
+		)
+		gomega.Expect(out).Should(gomega.ContainSubstring("L1 is successfully deployed on Local Network"))
+		gomega.Expect(err).Should(gomega.BeNil())
+
+		// stop the network
+		err = commands.StopNetwork()
+		gomega.Expect(err).Should(gomega.BeNil())
+
+		// now restart the network
+		out, err = commands.StartNetwork()
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(out).Should(gomega.ContainSubstring("Network ready to use"))
+
+		// check L1 status - should be back and running
+		out, err = utils.TestCommand(
+			cmd.BlockchainCmd,
+			"stats",
+			[]string{
+				testSubnetName,
+				"--local",
+				"--" + constants.SkipUpdateFlag,
+			},
+			utils.GlobalFlags{},
+			utils.TestFlags{},
+		)
+		gomega.Expect(err).Should(gomega.BeNil())
+		gomega.Expect(out).Should(gomega.ContainSubstring("already validating the subnet"))
+
+		commands.DeleteSubnetConfig(testSubnetName)
+	})
+
+	ginkgo.It("can start with given avalanchego version", func() {
+		testSubnetName := "testSubnet3"
+		// subnet config
+		_, avagoVersion := commands.CreateSubnetEvmConfigSOV(testSubnetName, utils.SubnetEvmGenesisPath)
+
+		// local network
+		_, err := commands.StartNetworkWithParams(map[string]interface{}{
+			"avalanchego-version": avagoVersion,
+		})
+		gomega.Expect(err).Should(gomega.BeNil())
+
+		_ = utils.DeleteConfigs(testSubnetName)
 	})
 })
