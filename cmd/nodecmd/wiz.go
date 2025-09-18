5d61bffca0046207b32fbcbabba6a198a18e93bb
Raymond Sukanto <rsukanto@umich.edu>
2025-09-18 18:19:57 -0400

    fix

--- cmd/nodecmd/wiz.go
+++ cmd/nodecmd/wiz.go
@@ -123,7 +123,7 @@ The node wiz command creates a devnet and deploys, sync and validate a subnet in
 	cmd.Flags().StringVar(&customVMRepoURL, "custom-vm-repo-url", "", "custom vm repository url")
 	cmd.Flags().StringVar(&customVMBranch, "custom-vm-branch", "", "custom vm branch or commit")
 	cmd.Flags().StringVar(&customVMBuildScript, "custom-vm-build-script", "", "custom vm build-script")
-	cmd.Flags().StringVar(&customVMPath, "custom-vm-branch", "", "custom vm branch or commit")
+	cmd.Flags().StringVar(&customVMPath, "custom-vm-path", "", "custom vm branch or commit")
 	cmd.Flags().StringVar(&customGrafanaDashboardPath, "add-grafana-dashboard", "", "path to additional grafana dashboard json file")
 	cmd.Flags().StringVar(&nodeConf, "node-config", "", "path to avalanchego node configuration for subnet")
 	cmd.Flags().StringVar(&subnetConf, "subnet-config", "", "path to the subnet configuration for subnet")
