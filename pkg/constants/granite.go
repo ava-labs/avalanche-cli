327e83c1545dc46fc07ffd0f5686b104dea072ed
Raymond Sukanto <rsukanto@umich.edu>
2025-10-13 18:39:57 -0400

    granite devnet

---
+++ pkg/constants/granite.go
@@ -0,0 +1,38 @@
+// Copyright (C) 2022, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+package constants
+
+import (
+	_ "embed"
+	"time"
+)
+
+//go:embed graniteDevnet/genesis.json
+var GraniteDevnetGenesisData []byte
+
+//go:embed graniteDevnet/upgrade.json
+var GraniteDevnetUpgradeData []byte
+
+const (
+	GraniteDevnetEndpoint  = "https://granite.avax-dev.network"
+	GraniteDevnetNetworkID = uint32(76)
+)
+
+var (
+	GraniteDevnetBootstrapNodeIDs = []string{
+		"NodeID-DG4zG5SNeMof4Fh6u5Dmn7qhpQD5pqbw1",
+		"NodeID-HDkaXpmjrENuBmqkggonVQh6tmuFuJFP",
+		"NodeID-PnfqkMjddeFVShZq8dwxb9KxsHgkES7RC",
+		"NodeID-5T26eUHiPv276fQuM4y8dDsJrVJkHFm3L",
+		"NodeID-M7StQdVYYzqWGHVZJsZoWjLWiWugB79DX",
+	}
+	GraniteDevnetBootstrapIPs = []string{
+		"107.21.11.213:9651",
+		"34.233.248.130:9651",
+		"52.201.126.172:9651",
+		"35.170.144.5:9651",
+		"98.82.41.186:9651",
+	}
+)
+
+const StakingGraniteMinimumDuration = 100 * time.Second
